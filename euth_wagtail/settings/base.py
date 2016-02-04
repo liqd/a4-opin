f9a4c0116615ca4bf2850b2795d849508f0270de
Caroline Clifford <me@carolineclifford.com>
2016-02-04 18:20:26 +0100

    Added styles for project page

--- euth_wagtail/settings/base.py
+++ euth_wagtail/settings/base.py
@@ -142,7 +142,8 @@ BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, 'euth_wagtail/static/third-party'
 
 BOWER_INSTALLED_APPS = (
     'jquery',
-    'bootstrap-sass'
+    'bootstrap-sass',
+    'salvattore'
 )
 
 STATICFILES_DIRS = [
