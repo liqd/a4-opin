5281b3b78ac349304204b017cf871ef6ff7da7e5
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-06-06 16:53:25 +0200

    clean up

--- comments_api/templatetags/react_comments.py
+++ comments_api/templatetags/react_comments.py
@@ -3,11 +3,22 @@ from django.contrib.contenttypes.models import ContentType
 
 register = template.Library()
 
+
 @register.inclusion_tag('comments_api/react_comments.html', takes_context=True)
 def react_comments(context, obj):
 
-	contenttype = ContentType.objects.get_for_model(obj)
-	pk = obj.pk
-	is_authenticated = context['request'].user.is_authenticated()
-	user_name = context['request'].user.username
-	return {'contenttype': contenttype.pk, 'pk': pk, 'is_authenticated':is_authenticated, 'user_name': user_name}
+    comments_conetenttype = content_type = ContentType.objects.get(
+            app_label="django_comments", model="comment")
+    contenttype = ContentType.objects.get_for_model(obj)
+    pk = obj.pk
+    is_authenticated = int(context['request'].user.is_authenticated())
+    user_name = context['request'].user.username
+    context = {
+        'obj': obj,
+        'comments_conetenttype' : comments_conetenttype,
+        'contenttype': contenttype.pk,
+        'pk': pk,
+        'is_authenticated': is_authenticated,
+        'user_name': user_name
+    }
+    return context
