905f88e306a2e3c413eb3a819533183c2ead7f1d
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-06-06 16:53:04 +0200

    update comments.js (styling, permissions, translations)

--- comments_api/static/comments_api/react_comments.js
+++ comments_api/static/comments_api/react_comments.js
@@ -1,15 +1,3 @@
-/**
- * This file provided by Facebook is for non-commercial testing and evaluation
- * purposes only. Facebook reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 (function () {
   function getCookie(c_name)
   {
@@ -33,32 +21,19 @@
     });
   });
 
-  var Comment = React.createClass({
-    rawMarkup: function() {
-      var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
-      return { __html: rawMarkup };
-    },
-
-    render: function() {
-      return (
-        h('div.comment', [
-          h('h2.commentAuthor', this.props.userName),
-          h('span', {
-            dangerouslySetInnerHTML: this.rawMarkup()
-          })
-        ])
-      );
-    }
-  });
-
   var CommentBox = React.createClass({
     loadCommentsFromServer: function() {
       $.ajax({
         url: this.props.url,
         dataType: 'json',
         cache: false,
+        data: {
+          object_pk: this.props.subjectId,
+          content_type: this.props.subjectType
+        },
         success: function(data) {
-          this.setState({data: data});
+          var commentCount = data.length;
+          this.setState({data: data, commentCount: commentCount});
         }.bind(this),
         error: function(xhr, status, err) {
           console.error(this.props.url, status, err.toString());
@@ -73,8 +48,9 @@
         data: comment,
         success: function(new_comment) {
           var comments = this.state.data;
-          var newComments = comments.concat([new_comment]);
-          this.setState({data: newComments});
+          var newComments = [new_comment].concat(comments);
+          var newCommentcount = newComments.length;
+          this.setState({data: newComments, commentCount: newCommentcount});
         }.bind(this),
         error: function(xhr, status, err) {
           console.error(this.props.url, status, err.toString());
@@ -82,23 +58,35 @@
       });
     },
     getInitialState: function() {
-      return {data: []};
+      return {data: [], commentCount: 0};
     },
     componentDidMount: function() {
       this.loadCommentsFromServer();
       setInterval(this.loadCommentsFromServer, this.props.pollInterval);
     },
     render: function() {
-      return (
-        h('div.commentBox', [
-          h('h1', 'Comments'),
-          h(CommentList, { data: this.state.data }),
-          h(CommentForm, { subjectType: this.props.subjectType,
-                           subjectId: this.props.subjectId,
-                           onCommentSubmit: this.handleCommentSubmit
-                         }),
-        ])
-      );
+      if(this.props.isAuthenticated){
+        return (
+          h('div.commentBox', [
+            h('div.comments_count', this.state.commentCount + " " + i18n_comments),
+            h(CommentForm, { subjectType: this.props.subjectType,
+                             subjectId: this.props.subjectId,
+                             onCommentSubmit: this.handleCommentSubmit
+                           }),
+            h(CommentList, { data: this.state.data })
+          ])
+        );
+      }
+      else {
+        return (
+           h('div.commentBox', [
+            h('div.comments_count', this.state.commentCount + " " + i18n_comments),
+            h('div.comments_login', i18n_please_loggin),
+            h(CommentList, { data: this.state.data })
+          ])
+
+        );
+      }
     }
   });
 
@@ -106,7 +94,7 @@
     render: function() {
       var commentNodes = this.props.data.map(function(comment) {
         return (
-          h(Comment, { userName: comment.user_name }, comment.comment)
+          h(Comment, { userName: comment.user_name, child_comments: comment.child_comments, submission_date: comment.submit_date}, comment.comment)
         );
       });
 
@@ -116,9 +104,31 @@
     }
   });
 
+  var Comment = React.createClass({
+    rawMarkup: function() {
+      var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+      return { __html: rawMarkup };
+    },
+
+    render: function() {
+      return (
+        h('div.comment', [
+          h('h3.commentAuthor', this.props.userName),
+          h('span', {
+            dangerouslySetInnerHTML: this.rawMarkup()
+          }),
+          h('div.commentSubmissionDate', this.props.submission_date),
+          h('div.child_comments_list', [
+            h(CommentList, { data: this.props.child_comments })
+          ])
+        ])
+      );
+    }
+  });
+
   var CommentForm = React.createClass({
     getInitialState: function() {
-      return {comment: '' };
+      return {comment: ''};
     },
     handleTextChange: function(e) {
       this.setState({comment: e.target.value});
@@ -133,18 +143,22 @@
         comment: comment,
         object_pk: this.props.subjectId,
         content_type: this.props.subjectType});
-      this.setState({comment: ''});
+        this.setState({comment: ''});
     },
     render: function() {
       return (
-        h('form.commentForm', { onSubmit: this.handleSubmit }, [
-          h('input', {
-            type: 'text',
-            placeholder: 'Say somehting..',
-            value: this.state.comment,
-            onChange: this.handleTextChange
-          }),
-          h('input', {
+        h('form', { onSubmit: this.handleSubmit }, [
+          h('div.form-group', [
+            h('textarea.form-control', {
+              type: 'text',
+              placeholder: i18n_your_comment,
+              rows: 5,
+              value: this.state.comment,
+              onChange: this.handleTextChange,
+              required: 'required'
+            })
+          ]),
+          h('input.btn.btn-primary', {
             type: 'submit',
             value: 'Post'
           })
@@ -155,13 +169,14 @@
 
   window._opin = window._opin || {}
 
-  window._opin.renderComment = function (subjectType, subjectId, target) {
+  window._opin.renderComment = function (subjectType, subjectId, isAuthenticated, target) {
     ReactDOM.render(
       h(CommentBox, {
         url: '/api/comments/',
         subjectType: subjectType,
         subjectId: subjectId,
-        pollInterval: 2000
+        isAuthenticated: isAuthenticated,
+        pollInterval: 20000,
       }),
       document.getElementById(target));
   }
