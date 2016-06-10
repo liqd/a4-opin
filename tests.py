14140afaf483af068c230b489aef40f22b71169e
Yves Müller <yves.mueller@liqd.de>
2016-06-10 16:32:33 +0200

    Add test to check if migrations are added

--- tests.py
+++ tests.py
@@ -21,3 +21,35 @@ class FormatingTestCase(TestCase):
         )
 
         self.assertFalse(changed)
+
+    def test_migrations(self):
+        """
+        Check if all migrations have been created.
+        """
+        from django.apps import apps
+        from django.conf import settings
+        from django.db.migrations.autodetector import MigrationAutodetector
+        from django.db.migrations.executor import MigrationExecutor
+        from django.db.migrations.state import ProjectState
+        from django.db import connections
+        from django.db.utils import OperationalError
+
+        changed = set()
+
+        for db in settings.DATABASES.keys():
+            try:
+                executor = MigrationExecutor(connections[db])
+            except OperationalError:
+                sys.exit(
+                    "Unable to check migrations: cannot connect to database\n"
+                )
+
+            autodetector = MigrationAutodetector(
+                executor.loader.project_state(),
+                ProjectState.from_apps(apps),
+            )
+
+            changed.update(
+                autodetector.changes(graph=executor.loader.graph).keys()
+            )
+            self.assertEqual(len(changed), 0)
