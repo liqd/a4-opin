c7d653a4bb695d372c3d8af8781dd4dba7274842
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-06-16 17:44:27 +0200

    Add first user_management test
     - test login forms

--- euth/user_management/tests.py
+++ euth/user_management/tests.py
@@ -1,6 +1,52 @@
+import pytest
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import AnonymousUser, User, Permission
 from django.test import TestCase
+from .forms import LoginForm
 
-# Create your tests here.
-class DummyTestCase(TestCase):
-    def test_dummy(self):
-        self.assertTrue(True)
+
+from .views import register_user
+from .views import login_user
+
+@pytest.fixture
+def testuser(db):
+    testuser = User.objects.create_user(
+        username='testuser',
+        password='password')
+    return testuser
+
+
+@pytest.mark.django_db
+def test_get_login_view(rf):
+    login_url = reverse('login')
+    request = rf.get(login_url)
+    request.user = AnonymousUser()
+    response = login_user(request)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_post_login_view(client, testuser):
+    login_url = reverse('login')
+    response = client.post(
+        login_url, {'username': 'testuser', 'password': 'password'})
+    assert response.status_code == 302
+    assert int(client.session['_auth_user_id']) == testuser.pk
+
+
+@pytest.mark.django_db
+def test_form_valid_login(rf, testuser):
+    request = rf.post('', {'username': 'testuser', 'password': 'password'})
+    form = LoginForm(request.POST)
+    assert form.is_valid() == True
+    user = form.login(request)
+    assert user == testuser
+
+
+@pytest.mark.django_db
+def test_form_invalid_login(rf, testuser):
+    request = rf.post('', {'username': 'testuser', 'password': 'wrong_password'})
+    form = LoginForm(request.POST)
+    assert form.is_valid() == False
+    assert (dict(form.errors))['__all__'] == ['password mismatch']
+
