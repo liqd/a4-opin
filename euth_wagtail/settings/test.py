251a4b6db23c964d0cf280f6892833547e63dfea
Yves Müller <slomo@users.noreply.github.com>
2016-06-23 16:09:00 +0200

    Revert "Disable all wagtail apps during pytest testing"

--- euth_wagtail/settings/test.py
+++
@@ -1,7 +0,0 @@
-from .dev import *
-
-
-wagtail_deps =  [ 'taggit', 'modelcluster' ]
-our_wagtail_apps = [ 'home', 'search', 'projects',  ]
-INSTALLED_APPS = [ app for app in INSTALLED_APPS
-                   if not 'wagtail.' in app and app not in our_wagtail_apps + wagtail_deps ]
