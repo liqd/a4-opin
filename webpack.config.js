f3928008587f39a7987c5eb2616a7e6b9da03475
Yves Müller <yves.mueller@liqd.de>
2016-06-28 09:14:37 +0200

    Exclude jquery from bundle
     - because bootstrap.js anyway requires jquery

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     path: './static/bundles/',
     filename: '[name]-[hash].js'
   },
+  externals: {
+    'jquery': 'jQuery'
+  },
   plugins: [
     new BundleTracker({filename: './webpack-stats.json'})
   ]
