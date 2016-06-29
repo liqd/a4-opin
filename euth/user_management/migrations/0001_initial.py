c1aa796db4b12ccb78cb3fedaab129809fbf2896
Yves Müller <yves.mueller@liqd.de>
2016-06-29 11:39:39 +0200

    Commit new migration for user_managment app

--- euth/user_management/migrations/0001_initial.py
+++ euth/user_management/migrations/0001_initial.py
@@ -2,9 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import model_utils.fields
+from django.conf import settings
+import django.utils.timezone
 import django.core.validators
-import django.contrib.auth.models
 import uuid
+import django.contrib.auth.models
 
 
 class Migration(migrations.Migration):
@@ -17,16 +20,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='User',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
-                ('password', models.CharField(verbose_name='password', max_length=128)),
-                ('last_login', models.DateTimeField(blank=True, verbose_name='last login', null=True)),
-                ('is_superuser', models.BooleanField(help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status', default=False)),
-                ('username', models.CharField(help_text='Required. 60 characters or fewer. Letters, digits, spaces and @/./+/-/_ only.', error_messages={'unique': 'A user with that username already exists.'}, unique=True, max_length=60, verbose_name='username', validators=[django.core.validators.RegexValidator('^[ \\w.@+-]+$', 'Enter a valid username. This value may contain only letters, digits, spacesand @/./+/-/_ characters.', 'invalid')])),
-                ('email', models.EmailField(error_messages={'unique': 'A user with that email address already exists.'}, verbose_name='email address', max_length=254, unique=True)),
-                ('is_staff', models.BooleanField(help_text='Designates whether the user can log into this admin site.', verbose_name='staff status', default=False)),
-                ('is_active', models.BooleanField(help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active', default=True)),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', to='auth.Group', related_query_name='user', verbose_name='groups', related_name='user_set')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', to='auth.Permission', related_query_name='user', verbose_name='user permissions', related_name='user_set')),
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(null=True, verbose_name='last login', blank=True)),
+                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('username', models.CharField(help_text='Required. 60 characters or fewer. Letters, digits, spaces and @/./+/-/_ only.', verbose_name='username', unique=True, max_length=60, error_messages={'unique': 'A user with that username already exists.'}, validators=[django.core.validators.RegexValidator('^[\\w]+[ \\w.@+-]*$', 'Enter a valid username. This value may contain only letters, digits, spaces and @/./+/-/_ characters. It must start with a digit or a letter.', 'invalid')])),
+                ('email', models.EmailField(unique=True, max_length=254, error_messages={'unique': 'A user with that email address already exists.'}, verbose_name='email address')),
+                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
+                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('date_joined', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, editable=False)),
+                ('groups', models.ManyToManyField(help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', to='auth.Group', verbose_name='groups', blank=True, related_name='user_set', related_query_name='user')),
+                ('user_permissions', models.ManyToManyField(help_text='Specific permissions for this user.', to='auth.Permission', verbose_name='user permissions', blank=True, related_name='user_set', related_query_name='user')),
             ],
             options={
                 'abstract': False,
@@ -38,12 +42,21 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Registration',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
-                ('token', models.UUIDField(default=uuid.uuid4, unique=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('token', models.UUIDField(unique=True, default=uuid.uuid4)),
                 ('username', models.TextField(max_length=255)),
                 ('email', models.EmailField(max_length=254)),
                 ('password', models.TextField(max_length=128)),
-                ('next_action', models.URLField(blank=True, null=True)),
+                ('next_action', models.URLField(null=True, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Reset',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('token', models.UUIDField(unique=True, default=uuid.uuid4)),
+                ('next_action', models.URLField(null=True, blank=True)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
