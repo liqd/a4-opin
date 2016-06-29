6e5b66a7cda95973236ca838bbdb9d858d9ed677
Yves Müller <yves.mueller@liqd.de>
2016-06-29 14:12:21 +0200

    Register user_managment model to admin UI

--- euth/user_management/admin.py
+++ euth/user_management/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.User)
+admin.site.register(models.Registration)
+admin.site.register(models.Reset)
