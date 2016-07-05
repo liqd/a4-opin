e487938423876ba27f6890be77a46606931ea48b
Yves Müller <yves.mueller@liqd.de>
2016-07-05 14:22:51 +0200

    Add file size check for image

--- euth/contrib/validators.py
+++ euth/contrib/validators.py
@@ -1,9 +1,14 @@
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
+image_max_mb = 5
 
 def validate_image(image, min_width, min_height):
     errors = []
+    image_max_size = image_max_mb * 10**6
+    if image.size > image_max_size:
+        msg = _('Image should be at most {max_size} MB')
+        errors += ValidationError(msg.format(max_size=image_max_mb))
     if image.width < min_width:
         msg = _('Image must be at least {min_width} pixels wide')
         errors += ValidationError(msg.format(min_width=min_width))
