8d5324f30238001dfe5c944381eecfb9869d3ca6
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-07-07 13:38:36 +0200

    add documentation list view

--- euth/organisations/urls.py
+++ euth/organisations/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.OrganisationListView.as_view(), name='organisation-list'),
     url(r'^(?P<slug>[-\w_]+)/$', views.OrganisationDetailView.as_view(), name='organisation-detail'),
 ]
--- euth/organisations/views.py
+++ euth/organisations/views.py
@@ -3,5 +3,10 @@ from django.views import generic
 
 from . import models
 
+
 class OrganisationDetailView(generic.DetailView):
+    model = models.Organisation
+
+
+class OrganisationListView(generic.ListView):
     model = models.Organisation
