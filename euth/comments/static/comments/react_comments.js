9725a9313c9efd5f2e194cf5505a83dd22e42992
Yves Müller <yves.mueller@liqd.de>
2016-07-08 12:14:39 +0200

    Replace self written cookie parser with lib

--- euth/comments/static/comments/react_comments.js
+++ euth/comments/static/comments/react_comments.js
@@ -4,23 +4,11 @@ var ReactDOM = require('react-dom')
 var h = require('react-hyperscript')
 var marked = require('marked')
 var moment = require('moment')
-
-var getCookie = function getCookie (c_name) {
-  if (document.cookie.length > 0) {
-    var c_start = document.cookie.indexOf(c_name + '=')
-    if (c_start != -1) {
-      c_start = c_start + c_name.length + 1
-      var c_end = document.cookie.indexOf(';', c_start)
-      if (c_end == -1) c_end = document.cookie.length
-      return unescape(document.cookie.substring(c_start, c_end))
-    }
-  }
-  return ''
-}
+var cookie = require('js-cookie')
 
 $(function () {
   $.ajaxSetup({
-    headers: { 'X-CSRFToken': getCookie('csrftoken') }
+    headers: { 'X-CSRFToken': cookie.get('csrftoken') }
   })
 })
 
