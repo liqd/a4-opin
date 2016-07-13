36dfa0f19309e6b8c3f2ae6ae57e9cf628ebfb17
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-07-13 12:19:55 +0200

    use autoslug

--- euth/ideas/models.py
+++ euth/ideas/models.py
@@ -1,10 +1,14 @@
+from autoslug import AutoSlugField
+
 from django.db import models
 from euth.modules import models as module_models
+from django.utils.functional import cached_property
+
 from euth.contrib import validators
 
 
 class Idea(module_models.Item):
-    slug = models.SlugField(max_length=512, unique=True)
+    slug = AutoSlugField(populate_from='name', unique=True)
     name = models.CharField(max_length=512)
     description = models.TextField()
     image = models.ImageField(upload_to='ideas/images', blank=True,
@@ -12,3 +16,11 @@ class Idea(module_models.Item):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        from django.core.urlresolvers import reverse
+        return reverse('idea-detail', args=[str(self.slug)])
+
+    @cached_property
+    def project(self):
+        return self.module.project
