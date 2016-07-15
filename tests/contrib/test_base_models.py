3d79a0baabcc6fcdc50c5b1a61d1c410329d8032
Yves Müller <yves.mueller@liqd.de>
2016-07-15 18:41:05 +0200

    Remove all remaining usages of model_utils

--- tests/contrib/test_base_models.py
+++ tests/contrib/test_base_models.py
@@ -1,20 +1,18 @@
-from django.db import connection, models
-from django.db.models import base
-from django.core.management.color import no_style
-from django.utils import timezone
-
 import freezegun
 import pytest
+from django.core.management.color import no_style
+from django.db import connection
+from django.db.models import base
+from django.utils import timezone
 
 from euth.contrib import base_models
 
-@pytest.fixture
-def time_stamped_model(request, db):
-    mixin = base_models.TimeStampedModel
+
+def derive_mixin(request, mixin):
     model = base.ModelBase(
         '__TestModel__' + mixin.__name__,
         (mixin,),
-        { '__module__': mixin.__module__ })
+        {'__module__': mixin.__module__})
     style = no_style()
     sql, _ = connection.creation.sql_create_model(model, style)
     cursor = connection.cursor()
@@ -30,6 +28,14 @@ def time_stamped_model(request, db):
     request.addfinalizer(fin)
     return model
 
+
+@pytest.fixture
+def time_stamped_model(request):
+    mixin = base_models.TimeStampedModel
+    return derive_mixin(request, mixin)
+
+
+@pytest.mark.django_db
 def test_model(time_stamped_model):
     TestModel = time_stamped_model
 
@@ -37,7 +43,7 @@ def test_model(time_stamped_model):
         obj = TestModel.objects.create()
         assert obj.created == timezone.now()
         created = timezone.now()
-        assert obj.modified == None
+        assert obj.modified is None
 
     with freezegun.freeze_time("2016-06-12"):
         obj = TestModel.objects.first()
