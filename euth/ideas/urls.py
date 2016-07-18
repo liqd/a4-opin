c544820170101952f7b470998c51ffda6029bdeb
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-07-18 19:29:44 +0200

    create ideas only allowed for users which are logged in

--- euth/ideas/urls.py
+++ euth/ideas/urls.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.conf.urls import include, url
 
 from . import views
 
 urlpatterns = [
-    url(r'create/module/(?P<slug>[-\w_]+)/$', views.IdeaCreateView.as_view(), name='idea-create'),
+    url(r'create/module/(?P<slug>[-\w_]+)/$', login_required(views.IdeaCreateView.as_view()), name='idea-create'),
     url(r'^(?P<slug>[-\w_]+)/edit/$', views.IdeaUpdateView.as_view(), name='idea-update'),
     url(r'^(?P<slug>[-\w_]+)/$', views.IdeaDetailView.as_view(), name='idea-detail'),
 ]
