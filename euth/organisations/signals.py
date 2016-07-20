6f67384f461df8e36c4bbf4887c4aa18e933745e
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-07-20 12:11:24 +0200

    delete thumbnails

--- euth/organisations/signals.py
+++ euth/organisations/signals.py
@@ -1,10 +1,15 @@
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
+from easy_thumbnails.files import get_thumbnailer
 
 from .models import Organisation
 
 
 @receiver(post_delete, sender=Organisation)
 def delete_images_for_Organusation(sender, instance, **kwargs):
+    thumbnailer = get_thumbnailer(instance.image)
+    thumbnailer.delete_thumbnails()
+    thumbnailer = get_thumbnailer(instance.logo)
+    thumbnailer.delete_thumbnails()
     instance.image.delete(False)
     instance.logo.delete(False)
--- euth/projects/signals.py
+++ euth/projects/signals.py
@@ -1,9 +1,12 @@
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
+from easy_thumbnails.files import get_thumbnailer
 
 from .models import Project
 
 
 @receiver(post_delete, sender=Project)
 def delete_images_for_Project(sender, instance, **kwargs):
+    thumbnailer = get_thumbnailer(instance.image)
+    thumbnailer.delete_thumbnails()
     instance.image.delete(False)
