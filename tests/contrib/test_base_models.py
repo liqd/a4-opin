04da1ee861892672a58c0b42d507c1f8c46f4b94
Markus Zapke-Gründemann <markus@transcode.de>
2016-07-20 15:44:53 +0200

    contrib: Revised test_time_stamped_model()
    Now a single test is used to test the whole create/update workflow. Additional
    assertions make sure that all model attributes have the right values and that
    the test is easy to understand.

--- tests/contrib/test_base_models.py
+++ tests/contrib/test_base_models.py
@@ -4,19 +4,18 @@ import freezegun
 import pytest
 
 
-class TestTimeStampedModel:
-
-    def test_created(self, time_stamped_model):
-        with freezegun.freeze_time('2016-06-06'):
-            obj = time_stamped_model()
-            assert obj.modified is None
-            assert obj.created == timezone.now()
-
-    @pytest.mark.django_db
-    def test_updated(self, time_stamped_model):
-        with freezegun.freeze_time('2016-06-06'):
-            obj = time_stamped_model()
-            obj.save()
-            assert obj.modified is None
-            obj.save()
-            assert obj.created == obj.modified == timezone.now()
+@pytest.mark.django_db
+def test_time_stamped_model(time_stamped_model):
+    with freezegun.freeze_time('2016-06-06 12:00:00'):
+        obj = time_stamped_model()
+        assert obj.pk is None
+        assert obj.created == timezone.now()
+        assert obj.modified is None
+        obj.save()
+        assert obj.pk is not None
+        assert obj.created == timezone.now()
+        assert obj.modified is None
+    with freezegun.freeze_time('2016-06-06 12:05:00'):
+        obj.save()
+        assert obj.created < timezone.now()
+        assert obj.modified == timezone.now()
