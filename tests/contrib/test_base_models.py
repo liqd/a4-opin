0881d55a0d4fc3e5a9b28e204c1bae962be8e602
Markus Zapke-Gründemann <markus@transcode.de>
2016-07-20 12:06:38 +0200

    contrib: Revise and split tests for TimeStampedModel

--- tests/contrib/test_base_models.py
+++ tests/contrib/test_base_models.py
@@ -1,51 +1,22 @@
-import freezegun
-import pytest
-from django.core.management.color import no_style
-from django.db import connection
-from django.db.models import base
 from django.utils import timezone
 
-from euth.contrib import base_models
+import freezegun
+import pytest
 
 
-def derive_mixin(request, mixin):
-    model = base.ModelBase(
-        '__TestModel__' + mixin.__name__,
-        (mixin,),
-        {'__module__': mixin.__module__})
-    style = no_style()
-    sql, _ = connection.creation.sql_create_model(model, style)
-    cursor = connection.cursor()
+class TestTimeStampedModel:
 
-    for statement in sql:
-        cursor.execute(statement)
+    def test_created(self, time_stamped_model):
+        with freezegun.freeze_time('2016-06-06'):
+            obj = time_stamped_model()
+            assert obj.modified is None
+            assert obj.created == timezone.now()
 
-    def fin():
-        sql = connection.creation.sql_destroy_model(model, (), style)
-        for statement in sql:
-            cursor.execute(statement)
-
-    request.addfinalizer(fin)
-    return model
-
-
-@pytest.fixture
-def time_stamped_model(request):
-    mixin = base_models.TimeStampedModel
-    return derive_mixin(request, mixin)
-
-@pytest.mark.django_db
-def test_model(time_stamped_model):
-    TestModel = time_stamped_model
-
-    with freezegun.freeze_time("2016-06-11"):
-        obj = TestModel.objects.create()
-        assert obj.created == timezone.now()
-        created = timezone.now()
-        assert obj.modified is None
-
-    with freezegun.freeze_time("2016-06-12"):
-        obj = TestModel.objects.first()
-        obj.save()
-        assert obj.created == created
-        assert obj.modified == timezone.now()
+    @pytest.mark.django_db
+    def test_updated(self, time_stamped_model):
+        with freezegun.freeze_time('2016-06-06'):
+            obj = time_stamped_model()
+            obj.save()
+            assert obj.modified is None
+            obj.save()
+            assert obj.created == obj.modified == timezone.now()
