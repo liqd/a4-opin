1a8717ad6152f36db2a893466add7a6ec6a3466b
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-08-03 18:13:09 +0200

    refactor: remove signals.py

--- euth/rates/apps.py
+++ euth/rates/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class RatesConfig(AppConfig):
     name = 'euth.rates'
     label = 'euth_rates'
-
-    def ready(self):
-        import euth.rates.signals  # noqa:F401
