16712b65f77d246d9c4bdf24b0bb72a4cafb057b
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-08-03 14:18:51 +0200

    add model

---
+++ euth/rates/models.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+from euth.contrib.base_models import TimeStampedModel
+
+
+class Rate(TimeStampedModel):
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_pk = models.PositiveIntegerField()
+    content_object = GenericForeignKey(
+        ct_field="content_type", fk_field="object_pk")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return str(self.value)
+
+    def save(self, *args, **kwargs):
+        self.value = self._get_value(self.value)
+        return super().save(*args, **kwargs)
+
+    def _get_value(self, number):
+        if number > 1 or number < -1:
+            return 0
+        return number
