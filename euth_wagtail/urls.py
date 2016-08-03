51052e1333a97579d7ef20716ef068c9a1bff419
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-08-03 14:20:18 +0200

    refactor

--- euth_wagtail/urls.py
+++ euth_wagtail/urls.py
@@ -4,14 +4,16 @@ from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views.i18n import javascript_catalog
+from rest_framework import routers
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
-from euth.comments import urls as comments_urls
+from euth.comments.api import CommentViewSet
 from euth.ideas import urls as ideas_urls
 from euth.organisations import urls as organisations_urls
 from euth.projects import urls as projects_urls
+from euth.rates.api import RateViewSet
 from euth.user_management import urls as user_urls
 from search import urls as search_urls
 
@@ -19,11 +21,15 @@ js_info_dict = {
     'packages': ('euth.comments',),
 }
 
+router = routers.DefaultRouter()
+router.register(r'comments', CommentViewSet, base_name='comments')
+router.register(r'rates', RateViewSet, base_name='rates')
+
 urlpatterns = [
     url(r'^django-admin/', include(admin.site.urls)),
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
-    url(r'^api/', include(comments_urls)),
+    url(r'^api/', include(router.urls)),
     url(r'', include(user_urls)),
 ]
 
