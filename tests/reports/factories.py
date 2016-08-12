bacba13564ab4ee98b4b56a0dd92058cf8891657
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-08-12 16:23:42 +0200

    fix tests

--- tests/reports/factories.py
+++ tests/reports/factories.py
@@ -1,10 +1,5 @@
 import factory
-from django.contrib.contenttypes.models import ContentType
 from tests.factories import UserFactory
-
-
-def get_comments_contenttype():
-    return ContentType.objects.get(app_label="comments", model="comment")
 
 
 class ReportFactory(factory.django.DjangoModelFactory):
@@ -13,5 +8,4 @@ class ReportFactory(factory.django.DjangoModelFactory):
         model = 'euth_reports.Report'
 
     description = factory.Faker('text')
-    content_type = get_comments_contenttype()
     user = factory.SubFactory(UserFactory)
