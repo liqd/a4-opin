f6b71eb86f5b915e14b4f27436f141dc68847ea5
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-08-15 17:31:33 +0200

    send email to creator of comment that was reported

--- euth/reports/emails.py
+++ euth/reports/emails.py
@@ -40,3 +40,33 @@ def send_email_to_moderators(request, report):
             recievers, 'report_moderators', context)
 
 
+def send_email_to_creator(request, report):
+
+    obj = report.content_object
+    if obj:
+        creator_field = get_creator_field(obj)
+        if creator_field:
+            creator = getattr(obj, creator_field)
+            recievers = [creator.email]
+            name = obj._meta.verbose_name.title()
+
+            context = Context({
+                'site': Site.objects.get_current(),
+                'name': name,
+                'description': report.description
+            })
+
+            emails.send_email_with_template(
+                recievers, 'report_creator', context)
+
+
+def get_creator_field(obj):
+    fields = obj._meta.fields
+
+    for f in fields:
+        field_object, model, direct, m2m = obj._meta.get_field_by_name(f.name)
+        if not m2m and direct and isinstance(field_object, ForeignKey):
+            if field_object.rel.to == get_user_model():
+                return f.name
+
+    return None
