0080e4e498c55b83d8fa0a0614b5cdf3c500c92f
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-08-17 13:35:32 +0200

    update tests

--- tests/comments/conftest.py
+++ tests/comments/conftest.py
@@ -1,5 +1,7 @@
 from pytest_factoryboy import register
+from tests.rates import factories as rate_factories
 
 import factories
 
+register(rate_factories.RateFactory)
 register(factories.CommentFactory)
