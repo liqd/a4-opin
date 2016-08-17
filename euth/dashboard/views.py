3cc8bea831b3e3e6b4d96cfd5a6c07764b806303
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-08-17 13:01:37 +0200

    add user update form to dashboard

--- euth/dashboard/views.py
+++ euth/dashboard/views.py
@@ -1,11 +1,23 @@
+from braces.views import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.views import generic
 
+from euth.user_management import models as user_models
 
-class DashboardProfileView(generic.TemplateView):
 
+class DashboardProfileView(LoginRequiredMixin, generic.UpdateView):
+
+    model = user_models.User
     template_name = "euth_dashboard/profile_detail.html"
+    fields = ['avatar', 'email']
+
+    def get_object(self):
+        return get_object_or_404(user_models.User, pk=self.request.user.id)
+
+    def get_success_url(self):
+        return self.request.path
 
 
-class DashboardOverviewView(generic.TemplateView):
+class DashboardOverviewView(LoginRequiredMixin, generic.TemplateView):
 
     template_name = "euth_dashboard/dashboard_overview.html"
