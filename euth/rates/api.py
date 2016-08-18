6c10db3bd796e110133f08f4a2e3d7545e34d296
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-08-18 15:43:42 +0200

    disallow get requests on rates list and detail view

--- euth/rates/api.py
+++ euth/rates/api.py
@@ -1,4 +1,4 @@
-from rest_framework import filters, permissions, status, viewsets
+from rest_framework import filters, mixins, permissions, status, viewsets
 from rest_framework.response import Response
 
 from .models import Rate
@@ -6,7 +6,10 @@ from .permissions import IsUserOrReadOnly
 from .serializers import RateSerializer
 
 
-class RateViewSet(viewsets.ModelViewSet):
+class RateViewSet(mixins.CreateModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  viewsets.GenericViewSet):
 
     queryset = Rate.objects.all()
     serializer_class = RateSerializer
