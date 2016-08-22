d711c5c83af9b8bc971879d28a440f10a1649da4
Yves Müller <yves.mueller@liqd.de>
2016-08-22 15:07:45 +0200

    Add missing files to simplify rate api

--- euth/rates/api.py
+++ euth/rates/api.py
@@ -1,4 +1,4 @@
-from rest_framework import filters, mixins, permissions, status, viewsets
+from rest_framework import filters, mixins, permissions, viewsets
 from rest_framework.response import Response
 
 from .models import Rate
@@ -15,23 +15,10 @@ class RateViewSet(mixins.CreateModelMixin,
     permission_classes = (
         permissions.IsAuthenticatedOrReadOnly, IsUserOrReadOnly)
     filter_backends = (filters.DjangoFilterBackend,)
-    filter_fields = ('object_pk', 'content_type', 'user', 'value')
+    filter_fields = ('object_pk', 'content_type')
 
-    def create(self, request):
-        """
-        Sets the user of the request as user of the rate
-        """
-        serializer = RateSerializer(
-            data=request.data,
-            context={'request': request}
-        )
-
-        if serializer.is_valid():
-            serializer.save(user=self.request.user)
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
     def destroy(self, request, pk=None):
         """
