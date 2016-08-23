7731bc7593273822bc0759925ca13bbd45876bba
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-08-23 17:18:38 +0200

    move serializer method to model

--- euth/rates/models.py
+++ euth/rates/models.py
@@ -1,12 +1,16 @@
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 
 from euth.contrib.base_models import TimeStampedModel
 
 
 class Rate(TimeStampedModel):
+
+    POSITIVE = 1
+    NEGATIVE = -1
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_pk = models.PositiveIntegerField()
@@ -27,9 +31,35 @@ class Rate(TimeStampedModel):
         return super().save(*args, **kwargs)
 
     def _get_value(self, number):
-        if number > 1:
-            return 1
-        elif number < -1:
-            return -1
+        if number > self.POSITIVE:
+            return self.POSITIVE
+        elif number < self.NEGATIVE:
+            return self.NEGATIVE
         else:
             return number
+
+    def get_meta_info(self, user):
+
+        rates = Rate.objects.filter(
+            content_type=self.content_type, object_pk=self.object_pk)
+        positive_rates_on_same_object = rates.filter(
+            value=self.POSITIVE).count()
+        negative_rates_on_same_object = rates.filter(
+            value=self.NEGATIVE).count()
+
+        try:
+            user_rate_on_same_object = rates.get(user=user)
+            user_rate_on_same_object_value = user_rate_on_same_object.value
+            user_rate_on_same_object_id = user_rate_on_same_object.pk
+        except ObjectDoesNotExist:
+            user_rate_on_same_object_value = None
+            user_rate_on_same_object_id = None
+
+        result = {
+            'positive_rates_on_same_object': positive_rates_on_same_object,
+            'negative_rates_on_same_object': negative_rates_on_same_object,
+            'user_rate_on_same_object_value': user_rate_on_same_object_value,
+            'user_rate_on_same_object_id': user_rate_on_same_object_id
+        }
+
+        return result
