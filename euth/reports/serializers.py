4d65962e40df3dd8540be32189d161f2bf4ff0d1
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-08-23 13:56:23 +0200

    adapt serializer

--- euth/reports/serializers.py
+++ euth/reports/serializers.py
@@ -8,18 +8,3 @@ class ReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = Report
         exclude = ('user', 'modified', 'created')
-
-    def validate(self, attrs):
-
-        content_type = attrs.get('content_type')
-        object_pk = attrs.get('object_pk')
-        user = self.context['request'].user
-
-        try:
-            Report.objects.get(
-                content_type=content_type, object_pk=object_pk, user=user)
-        except Report.DoesNotExist:
-            return attrs
-        else:
-            raise serializers.ValidationError(
-                'user report on this item already exists')
