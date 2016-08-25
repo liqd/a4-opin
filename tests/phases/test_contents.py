e74aa608f3158e8cbc32b61ea99156fa6f734b75
Yves Müller <yves.mueller@liqd.de>
2016-08-25 11:12:47 +0200

    Refactor phase content tests

--- tests/phases/test_contents.py
+++ tests/phases/test_contents.py
@@ -1,28 +1,12 @@
+import pytest
 from tests.apps.blog import models
 from tests.apps.blog.phases import BlogPhase
 
 from euth.phases import PhaseContent, content
 
 
-def test_blogapp_phase_registered():
-    assert 'blog:020:phase' in content
-    assert content['blog:020:phase'].__class__ is BlogPhase
-    assert ('blog:020:phase', 'BlogPhase (blog:phase)') in content.as_choices()
-
-
-def test_phase_content():
-    class DummyPhase(PhaseContent):
-        app = 'dummy'
-        phase = 'phase'
-        weight = 9999
-
-    phase = DummyPhase()
-
-    assert phase.identifier == 'dummy:999:phase'
-    assert str(phase) == 'DummyPhase (dummy:phase)'
-
-
-def test_phase_features():
+@pytest.fixture
+def dummy_phase():
     class DummyPhase(PhaseContent):
         app = 'dummy'
         phase = 'phase'
@@ -31,5 +15,16 @@ def test_phase_features():
             'comment': (models.Post,)
         }
 
-    phase = DummyPhase()
-    assert phase.has_feature('comment', models.Post)
+    return DummyPhase()
+
+
+def test_blogapp_phase_registered():
+    assert 'blog:020:phase' in content
+    assert content['blog:020:phase'].__class__ is BlogPhase
+    assert ('blog:020:phase', 'BlogPhase (blog:phase)') in content.as_choices()
+
+
+def test_phase_content(dummy_phase):
+    assert dummy_phase.identifier == 'dummy:999:phase'
+    assert str(dummy_phase) == 'DummyPhase (dummy:phase)'
+    assert dummy_phase.has_feature('comment', models.Post)
