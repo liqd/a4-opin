bb51014b46ef2f80af9ce83190933187443af412
Yves Müller <yves.mueller@liqd.de>
2016-08-26 09:34:02 +0200

    Add basic project views to dashboard

--- euth/dashboard/forms.py
+++ euth/dashboard/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from euth.contrib import widgets
+from euth.projects import models as project_models
 from euth.user_management import models as user_models
 
 
@@ -11,3 +12,21 @@ class ProfileForm(forms.ModelForm):
         widgets = {
             'avatar': widgets.ImageInputWidget()
         }
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = project_models.Project
+        fields = ['image', 'name', 'description', 'information', 'is_public']
+
+
+class ExtendedProjectForm(ProjectForm):
+
+    invites = forms.CharField(required=False)
+
+    class Meta(ProjectForm.Meta):
+        fields = ProjectForm.Meta.fields + ['invites']
+
+    def save(self):
+        self.instance.is_draft = 'save_draft' in self.cleaned_data
+        return super().save()
