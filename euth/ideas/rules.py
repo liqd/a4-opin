59e3974d4f436839eab49d2e639870a2a223b70b
Yves Müller <yves.mueller@liqd.de>
2016-09-01 17:38:26 +0200

    Refactor rules and predicates
     - move all predicates to corresponding package

--- euth/ideas/rules.py
+++ euth/ideas/rules.py
@@ -1,54 +1,18 @@
 import rules
+from rules.predicates import is_superuser
 
-from euth.phases import content
+from euth.modules.predicates import (is_context_member, is_context_moderator,
+                                     is_owner)
+from euth.phases.predicates import phase_allows_create, phase_allows_modify
 
-from . import models
-
-
-@rules.predicate
-def is_active_phase(project, phase_type):
-    return[content.active_phase.type] == phase_type
+from .models import Idea
 
 
-@rules.predicate
-def is_moderator(user, project):
-    return user in project.moderators.all()
+rules.add_perm('ideas.modify_idea',
+               is_superuser | is_context_moderator |
+               (is_context_member & is_owner & phase_allows_modify))
 
 
-@rules.predicate
-def is_member(user, project):
-    return (
-        (project.is_public and user.is_authenticated())
-        or user in project.participants.all())
-
-
-def has_feature_active(project, model, feature):
-    if not project.active_phase:
-        return False
-    else:
-        return project.active_phase.has_feature('crud', model)
-
-
-@rules.predicate
-def is_owner(user, item):
-    return item.creator == user
-
-
-@rules.predicate
-def can_modify_idea(user, idea):
-    return (
-        is_moderator(user, idea.project) | (
-            is_owner(user, idea) &
-            is_member(user, idea.project) &
-            has_feature_active(idea.project, models.Idea, 'crud')))
-
-
-@rules.predicate
-def can_create_idea(user, module):
-    return (
-        is_moderator(user, module.project) | (
-            is_member(user, module.project) &
-            has_feature_active(module.project, models.Idea, 'crud')))
-
-rules.add_perm('ideas.modify_idea', can_modify_idea)
-rules.add_perm('ideas.create_idea', can_create_idea)
+rules.add_perm('ideas.create_idea',
+               is_superuser | is_context_moderator |
+               (is_context_member & phase_allows_create(Idea)))
