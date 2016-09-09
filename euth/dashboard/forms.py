c58b1b8570f633f9e5f3d0b0cd7282819918fd70
Yves Müller <yves.mueller@liqd.de>
2016-09-09 17:58:13 +0200

    Extend dashboard with basic invite handeling

--- euth/dashboard/forms.py
+++ euth/dashboard/forms.py
@@ -1,6 +1,10 @@
+import email.utils
+
 from django import forms
+from django.core.exceptions import ValidationError
 
 from euth.contrib import widgets
+from euth.memberships import models as member_models
 from euth.projects import models as project_models
 from euth.users import models as user_models
 
@@ -12,6 +16,32 @@ class ProfileForm(forms.ModelForm):
         widgets = {
             'avatar': widgets.ImageInputWidget()
         }
+
+
+class ProjectInviteForm(forms.Form):
+    emails = forms.CharField(required=False)
+
+    def __init__(self, project, *args, **kwargs):
+        self.project = project
+        super().__init__(*args, **kwargs)
+
+    def clean_emails(self):
+        emails_str = self.cleaned_data['emails']
+        emails = email.utils.getaddresses([emails_str])
+        addresses = [email[1] for email in emails]
+        query = {
+            'project': self.project,
+            'email__in': addresses,
+        }
+        existing = member_models.Invite.objects.filter(**query)\
+                                               .values_list('email', flat=True)
+        if existing:
+            for address in existing:
+                raise ValidationError(
+                    '{} already invited'.format(address)
+                )
+
+        return emails
 
 
 class ProjectForm(forms.ModelForm):
