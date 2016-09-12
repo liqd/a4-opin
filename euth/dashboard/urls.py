76ccea933d31725c8351a63b653c444ad6534569
Yves Müller <yves.mueller@liqd.de>
2016-09-12 12:41:03 +0200

    Make dashboard organisation aware

--- euth/dashboard/urls.py
+++ euth/dashboard/urls.py
@@ -3,19 +3,26 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^profile/$',
+    url(r'^$',
+        views.dashboard,
+        name='dashboard'),
+    url(r'^profile$',
         views.DashboardProfileView.as_view(),
         name='dashboard-profile'),
-    url(r'^projects/$',
+    url(r'^(?P<organisation_slug>[-\w_]+)/profile$',
+        views.DashboardProfileView.as_view(),
+        name='dashboard-profile-org'),
+    url(r'^(?P<organisation_slug>[-\w_]+)/projects/$',
         views.DashboardProjectListView.as_view(),
         name='dashboard-project-list'),
-    url(r'^projects/(?P<slug>[-\w_]+)/$',
+    url(r'^(?P<organisation_slug>[-\w_]+)/projects/(?P<slug>[-\w_]+)/$',
         views.DashboardProjectUpdateView.as_view(),
         name='dashboard-project-edit'),
-    url(r'^projects/(?P<slug>[-\w_]+)/users$',
+    url(r'^(?P<organisation_slug>[-\w_]+)/projects/(?P<slug>[-\w_]+)/users$',
         views.DashboardProjectUserView.as_view(),
         name='dashboard-project-users'),
-    url(r'^projects/(?P<slug>[-\w_]+)/users/invite$',
+    url(r'^(?P<organisation_slug>[-\w_]+)/projects/'
+        r'(?P<slug>[-\w_]+)/users/invite$',
         views.DashboardProjectInviteView.as_view(),
         name='dashboard-project-invite'),
 ]
