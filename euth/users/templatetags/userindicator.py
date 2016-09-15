eb0a284b4e3f035da23a14a0f78f04984844d9bf
Yves Müller <yves.mueller@liqd.de>
2016-09-15 12:42:42 +0200

    Rename next_action parameter consitently

--- euth/users/templatetags/userindicator.py
+++ euth/users/templatetags/userindicator.py
@@ -7,5 +7,5 @@ register = template.Library()
 
 @register.inclusion_tag('euth_users/indicator_menu.html', takes_context=True)
 def userindicator_menu(context):
-    context['next_action'] = sanatize_next(context['request'])
+    context['redirect_field_value'] = sanatize_next(context['request'])
     return context
