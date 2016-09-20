416f833922c139eb1aaa33967143693c7d469fc7
Yves Müller <yves.mueller@liqd.de>
2016-09-20 10:26:55 +0200

    Fix resolve 404 error in production setup
     - unable to reproduce in testsetup
     - might be related to i18n urls

--- euth/users/__init__.py
+++ euth/users/__init__.py
@@ -1,7 +1,7 @@
-from django.core.urlresolvers import resolve
+from django.core.urlresolvers import resolve, Resolver404
 
 
-def _get_invalid_url_names():
+def _get_account_url_names():
     from allauth.account import urls
     return tuple([url.name for url in urls.urlpatterns])
 
@@ -10,7 +10,12 @@ def sanatize_next(request):
     """
     Get appropriate next value for the given request
     """
-    if resolve(request.path).url_name in _get_invalid_url_names():
+    try:
+        url_name = resolve(request.path).url_name
+    except Resolver404:
+        url_name = '__invalid_url_name__'
+
+    if url_name in _get_account_url_names():
         next = request.GET.get('next') or request.POST.get('next') or '/'
     else:
         next = request.get_full_path()
