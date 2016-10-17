830698492dd1d73343a3209f0a3a8cc74aeb56c1
Yves Müller <yves.mueller@liqd.de>
2016-10-17 17:48:49 +0200

    Make flashpoll base url configurable

--- euth/flashpoll/views.py
+++ euth/flashpoll/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.utils.translation import get_language
 from django.views import generic
 
@@ -14,9 +15,10 @@ class FlashpollDetailView(mixins.ProjectMixin, generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = {
-            'url': 'http://flashpoll.opin.me/{}/poll/{}'.format(
-                get_language(),
-                kwargs['object'].key
+            'url': '{base_url}/{language}/poll/{poll_id}'.format(
+                base_url=settings.FLASHPOLL_URL,
+                language=get_language(),
+                poll_id=self.get_object().key
             )
         }
         return super().get_context_data(**context)
