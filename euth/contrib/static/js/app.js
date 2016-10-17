7c7a72fd8d83a5c4a9c49e9cfe14e1e84bac376e
Philipp <vellerphilipp@gmail.com>
2016-10-17 17:47:26 +0200

    changed .js to .jsx extension

--- euth/contrib/static/js/app.js
+++ euth/contrib/static/js/app.js
@@ -1,5 +1,5 @@
 var ReactComments = require('../../../comments/static/comments/react_comments.js')
-var ReactRatings = require('../../../ratings/static/ratings/react_ratings.js')
+var ReactRatings = require('../../../ratings/static/ratings/react_ratings.jsx')
 var ReactLanguageSwitch = require('../../../dashboard/static/language_switch/react_language_switch.jsx')
 
 module.exports = {
