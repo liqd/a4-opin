8d0a32a541c09ac3740d85a0f9b1c14fd16e2559
Yves Müller <yves.mueller@liqd.de>
2016-10-18 11:08:15 +0200

    move babelrc to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,14 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
     ]
   },
   plugins: [
