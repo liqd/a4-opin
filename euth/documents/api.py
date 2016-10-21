efb1b588825035137da3374c6cee81f7ed79c09b
Yves Müller <yves.mueller@liqd.de>
2016-10-21 11:30:37 +0200

    rely more on rest framework generic api set

--- euth/documents/api.py
+++ euth/documents/api.py
@@ -1,5 +1,4 @@
-from rest_framework import status, viewsets
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from euth.contrib.api.permissions import IsModerator
 
@@ -10,21 +9,6 @@ from .serializers import DocumentSerializer
 class DocumentViewSet(viewsets.ModelViewSet):
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
-
-    def create(self, request):
-        """
-        Sets the user of the request as user of the document
-        """
-        serializer = DocumentSerializer(
-            data=request.data,
-            context={'request': request})
-
-        if serializer.is_valid():
-            document = serializer.create(serializer.validated_data)
-            return Response(DocumentSerializer(document).data)
-        else:
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
     permission_classes = (
         IsModerator,
     )
