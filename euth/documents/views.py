6f0c2405fa4e15df7651a5f1330f7bc9e87f00b9
Yves Müller <yves.mueller@liqd.de>
2016-10-21 11:45:53 +0200

    reduce document create view to minimum
     - only extend views with minimal required functionality
     - use project specific mixin

--- euth/documents/views.py
+++ euth/documents/views.py
@@ -1,27 +1,17 @@
-from django.core.exceptions import ObjectDoesNotExist
 from django.views import generic
 from rules.contrib.views import PermissionRequiredMixin
 
 from euth.projects import mixins
-from euth.projects.models import Project
 
 from . import models
 
 
-class DocumentCreateView(generic.CreateView, PermissionRequiredMixin):
-    model = models.Document
-    fields = []
+class DocumentCreateView(mixins.ProjectMixin, generic.TemplateView):
+    template_name = 'euth_documents/document_form.html'
 
-    def dispatch(self, *args, **kwargs):
-        proj_slug = self.kwargs[self.slug_url_kwarg]
-        self.project = Project.objects.get(slug=proj_slug)
-        self.phase = self.project.active_phase
-        self.module = self.phase.module if self.phase else None
-        try:
-            self.document = models.Document.objects.get(module=self.module)
-        except ObjectDoesNotExist:
-            self.document = None
-        return super().dispatch(*args, **kwargs)
+    @property
+    def document(self):
+        return models.Document.objects.filter(module=self.module).first()
 
 
 class DocumentDetailView(generic.DetailView, mixins.ProjectMixin):
