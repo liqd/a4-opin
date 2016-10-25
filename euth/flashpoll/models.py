e2e49f1ef216fb3cb3fd95697a3f1c9af46aa342
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-10-25 16:29:10 +0200

    add verbose_name to flashpoll model

--- euth/flashpoll/models.py
+++ euth/flashpoll/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
 from euth.modules import models as module_models
 
 
 class Flashpoll(module_models.AbstractSettings):
-    key = models.CharField(max_length=30)
+    key = models.CharField(max_length=30, verbose_name=_('Flashpoll ID'))
 
     def __str__(self):
         return self.key
