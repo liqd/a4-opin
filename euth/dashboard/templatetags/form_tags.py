d23613ff5bcfca2f8819f50a1c5f58d7c4f56961
Yves Müller <yves.mueller@liqd.de>
2016-11-01 11:42:25 +0100

    replace is checkbox check with widget tweaks check

--- euth/dashboard/templatetags/form_tags.py
+++ euth/dashboard/templatetags/form_tags.py
@@ -1,14 +1,6 @@
 from django import template
-from django.forms import CheckboxInput
 
 register = template.Library()
-
-
-@register.filter(name='is_checkbox')
-def is_checkbox(field):
-    checkbox_class_name = CheckboxInput().__class__.__name__
-
-    return field.field.widget.__class__.__name__ == checkbox_class_name
 
 
 @register.assignment_tag
