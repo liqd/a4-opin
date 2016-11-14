00e1d6f6de60e12474fd2db7e1d32473add108b7
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-11-14 10:56:09 +0100

    update signal

--- euth/comments/signals.py
+++ euth/comments/signals.py
@@ -5,12 +5,17 @@ from django.dispatch import receiver
 from euth.actions.models import Action
 from euth.contrib import services
 
+from euth.projects.models import Project
+
 from .models import Comment
 
 
 @receiver(post_save, sender=Comment)
 def add_action(sender, instance, created, **kwargs):
     comment_contenttype = ContentType.objects.get_for_model(instance)
+    project = instance.project if hasattr(instance, 'project') and isinstance(
+        instance.project, Project) else None
+
     if created:
         Action.objects.create(
             actor=instance.creator,
@@ -18,7 +23,7 @@ def add_action(sender, instance, created, **kwargs):
             target_object_id=instance.object_pk,
             action_object_content_type=comment_contenttype,
             action_object_object_id=instance.pk,
-            project=instance.project,
+            project=project,
             verb='created'
         )
     else:
@@ -28,7 +33,7 @@ def add_action(sender, instance, created, **kwargs):
             target_object_id=instance.object_pk,
             action_object_content_type=comment_contenttype,
             action_object_object_id=instance.pk,
-            project=instance.project,
+            project=project,
             verb='updated'
         )
 
