07dc09c2af95db608cf00145b2a9a6109b00b76b
vellip <vellerphilipp@googlemail.com>
2016-11-18 12:19:04 +0100

    update url patter

--- euth/users/urls.py
+++ euth/users/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<slug>[-\w _]+)/$', views.ProfileView.as_view(),
+    url(r'^(?P<slug>[-\w _.@+-]+)/$', views.ProfileView.as_view(),
         name='profile'),
 ]
