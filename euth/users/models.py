f91eed7bbcfddb4ea571c2a2e5d19970cfb0c4cd
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-11-21 18:39:24 +0100

    add field for notifications

--- euth/users/models.py
+++ euth/users/models.py
@@ -24,6 +24,9 @@ EMAIL_NOT_UNIQUE = _('A user with that email address already exists.')
 IS_STAFF_HELP = _('Designates whether the user can log into this admin site.')
 IS_ACTIVE_HELP = _('Designates whether this user should be treated as active. '
                    'Unselect this instead of deleting accounts.')
+GET_NOTIFICATIONS_HELP = _('Designates whether you want to receive'
+                           'notifications. Unselect if you do not '
+                           'want to receive notifications.')
 
 
 class User(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
@@ -43,6 +46,9 @@ class User(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
                                    help_text=IS_STAFF_HELP)
     is_active = models.BooleanField(_('active'), default=True,
                                     help_text=IS_ACTIVE_HELP)
+    get_notifications = models.BooleanField(
+        _('get notifications'), default=True,
+        help_text=GET_NOTIFICATIONS_HELP)
     date_joined = models.DateTimeField(editable=False, default=timezone.now)
 
     _avatar = models.ImageField(
