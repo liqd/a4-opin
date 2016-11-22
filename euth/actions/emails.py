f4395e051a65ccdd86c87ac9271598860117863f
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-11-22 18:17:34 +0100

    add some additinal filters

--- euth/actions/emails.py
+++ euth/actions/emails.py
@@ -27,7 +27,10 @@ def notify_followers_on_almost_finished(project):
     translation.activate('en')
     User = auth.get_user_model()
     recipients = User.objects.filter(
-        follow__project=project).values_list('email', flat=True)
+        follow__project=project).filter(
+        follow__enabled=True).filter(
+        get_notifications=True
+    ).values_list('email', flat=True)
 
     context = {
         'project': project,
