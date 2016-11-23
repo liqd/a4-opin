3231ea9e04a33b13686398a9d0e726691496ba20
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-11-23 11:15:09 +0100

    dont sent notification if user commented on own resource

--- euth/actions/signals.py
+++ euth/actions/signals.py
@@ -10,7 +10,8 @@ from .models import Action
 def send_notification(sender, instance, created, **kwargs):
 
     if instance.verb == 'created' and hasattr(instance.target, 'creator'):
-        if instance.target.creator.get_notifications:
+        creator = instance.target.creator
+        if creator.get_notifications and not creator == instance.actor:
             emails.notify_creator_on_create_action(instance)
     if instance.verb == 'project almost finished':
         emails.notify_followers_on_almost_finished(instance.project)
