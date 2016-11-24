20e87862231dc48b069f9db2781eb91440bee42a
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-11-24 15:01:48 +0100

    add proposal detail

--- euth/proposals/urls.py
+++ euth/proposals/urls.py
@@ -1,5 +1,8 @@
+from django.conf.urls import url
 
+from . import views
 
 urlpatterns = [
-
+    url(r'^(?P<pk>\d+)/$',
+        views.ProposalDetailView.as_view(), name='proposal-detail')
 ]
--- euth/proposals/views.py
+++ euth/proposals/views.py
@@ -5,5 +5,9 @@ from euth.projects import mixins
 from .models import Proposal
 
 
+class ProposalDetailView(generic.DetailView):
+    model = Proposal
+
+
 class ProposalListView(mixins.ProjectMixin, generic.ListView):
     model = Proposal
