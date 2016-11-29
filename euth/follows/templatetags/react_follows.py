445cdc73e455df8c5bec411b775745b0b72f349f
vellip <vellerphilipp@googlemail.com>
2016-11-29 13:36:11 +0100

    add edit button to project detail page, rename rule to be conform with naming convention, make follow button in span instead for inline display

--- euth/follows/templatetags/react_follows.py
+++ euth/follows/templatetags/react_follows.py
@@ -15,7 +15,7 @@ def react_follows(project):
     mountpoint = 'follow_{}'.format(project.slug)
 
     return mark_safe((
-        '<div id={mountpoint}></div><script>window.opin.renderFollow('
+        '<span id={mountpoint}></span><script>window.opin.renderFollow('
         '{mountpoint}, {attributes})</script>').format(
             attributes=json.dumps(attributes),
             mountpoint=json.dumps(mountpoint)
