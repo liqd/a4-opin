4bcf8665394f8a4d8618aa2ab2b952c83335c9be
Yves Müller <yves.mueller@liqd.de>
2016-12-01 14:37:42 +0100

    reafactor __str__ method
     - drop translations
     - use python3 style format strings

--- euth/actions/models.py
+++ euth/actions/models.py
@@ -3,7 +3,6 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import ugettext as _
 
 from euth.projects.models import Project
 
@@ -12,7 +11,7 @@ from . import verbs
 
 class Action(models.Model):
 
-    # actor
+    # actor, if actor is None the action was create by the system
     actor = models.ForeignKey(settings.AUTH_USER_MODEL,
                               on_delete=models.CASCADE,
                               blank=True,
@@ -48,7 +47,7 @@ class Action(models.Model):
     def __str__(self):
 
         ctx = {
-            'actor': self.actor.username if self.actor else '',
+            'actor': self.actor.username if self.actor else 'system',
             'verb': self.verb,
             'action_object': self.action_object,
             'target': self.target
@@ -56,12 +55,9 @@ class Action(models.Model):
 
         if self.target:
             if self.action_object:
-                return _('%(actor)s %(verb)s '
-                         '%(action_object)s on '
-                         '%(target)s') % ctx
-            return _('%(actor)s %(verb)s '
-                     '%(target)s') % ctx
+                return '{actor} {verb} {action_object} on {target}'.format(
+                    **ctx)
+            return '{actor} {verb} {target}'.format(**ctx)
         if self.action_object:
-            return _('%(actor)s %(verb)s '
-                     '%(action_object)s') % ctx
-        return _('%(actor)s %(verb)s') % ctx
+            return '{actor} {verb} {action_object}'.format(**ctx)
+        return '{actor} {verb}'.format(**ctx)
