5a38892142caf9fbc81c9630df8f0d52e62e7274
Yves Müller <yves.mueller@liqd.de>
2016-12-02 17:58:05 +0100

    pass next parameter through activation flow
     - append next to email activation url
     - hack that next parameter is passed through email confirmation view
     - redirect after successful email confirmation

--- euth/users/adapters.py
+++ euth/users/adapters.py
@@ -8,5 +8,18 @@ from euth.users import USERNAME_REGEX
 class EuthAccountAdapter(DefaultAccountAdapter):
     username_regex = re.compile(USERNAME_REGEX)
 
+    def get_email_confirmation_url(self, request, emailconfirmation):
+        url = super().get_email_confirmation_url(request, emailconfirmation)
+        if 'next' in request.POST:
+            return '{}?next={}'.format(url, request.POST['next'])
+        else:
+            return url
+
     def send_mail(self, template_prefix, email, context):
         return send_email_with_template([email], template_prefix, context)
+
+    def get_email_confirmation_redirect_url(self, request):
+        if 'next' in request.POST:
+            return request.POST['next']
+        else:
+            super().get_email_confirmation_redirect_url(request)
