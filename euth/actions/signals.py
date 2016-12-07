f5e7a2ddbeb0ab1537bac461187cba4e4d78bf1b
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-12-07 18:46:25 +0100

    move logic from comments to actions

--- euth/actions/signals.py
+++ euth/actions/signals.py
@@ -14,6 +14,27 @@ def send_notification(sender, instance, created, **kwargs):
         creator = instance.target.creator
         if creator.get_notifications and not creator == instance.actor:
             emails.notify_creator_on_create_action(instance)
+def add_action(sender, instance, created, **kwargs):
+    content_type = ContentType.objects.get_for_model(instance)
+    verb = verbs.CREATE if created else verbs.UPDATE
+    actor = instance.creator
+
+    action = Action(
+        actor=actor,
+        verb=verb,
+        action_object_content_type=content_type,
+        action_object_object_id=instance.pk,
+    )
+
+    if str(content_type) == 'Comment':
+        project = instance.project if hasattr(
+            instance, 'project') and isinstance(
+            instance.project, Project) else None
+
+        action.target_content_type = instance.content_type
+        action.target_object_id = instance.object_pk
+        action.project = project
+        action.save()
 for app, model in settings.ACTIONABLE:
     post_save.connect(add_action, apps.get_model(app, model))
     if instance.verb == verbs.COMPLETE:
--- euth/comments/signals.py
+++ euth/comments/signals.py
@@ -1,42 +1,10 @@
 from django.contrib.contenttypes.models import ContentType
-from django.db.models.signals import post_delete, post_save
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
-from euth.actions import verbs
-from euth.actions.models import Action
 from euth.contrib import services
 
-from euth.projects.models import Project
-
 from .models import Comment
-
-
-@receiver(post_save, sender=Comment)
-def add_action(sender, instance, created, **kwargs):
-    comment_contenttype = ContentType.objects.get_for_model(instance)
-    project = instance.project if hasattr(instance, 'project') and isinstance(
-        instance.project, Project) else None
-
-    if created:
-        Action.objects.create(
-            actor=instance.creator,
-            target_content_type=instance.content_type,
-            target_object_id=instance.object_pk,
-            action_object_content_type=comment_contenttype,
-            action_object_object_id=instance.pk,
-            project=project,
-            verb=verbs.CREATE
-        )
-    else:
-        Action.objects.create(
-            actor=instance.creator,
-            target_content_type=instance.content_type,
-            target_object_id=instance.object_pk,
-            action_object_content_type=comment_contenttype,
-            action_object_object_id=instance.pk,
-            project=project,
-            verb=verbs.UPDATE
-        )
 
 
 @receiver(post_delete, sender=Comment)
