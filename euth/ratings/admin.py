d8f431300b755b00722422b7abdadaa17ab18142
Yves Müller <yves.mueller@liqd.de>
2016-12-09 13:35:08 +0100

    remove ratings from project

--- euth/ratings/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Rating
-
-admin.site.register(Rating)
--- euth/ratings/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RatingsConfig(AppConfig):
-    name = 'euth.ratings'
-    label = 'euth_ratings'
