e53b4ecb6efc742d4e1c593ba32c7212da03365c
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-12-13 18:22:54 +0100

    add map idea create form

--- euth/maps/views.py
+++ euth/maps/views.py
@@ -3,4 +3,12 @@ from .models import MapIdea
 
 
 class MapIdeaListView(generic.ListView):
+class MapIdeaCreateView(idea_views.IdeaCreateView):
+    model = MapIdea
+    form_class = forms.MapIdeaForm
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['settings_instance'] = self.module.settings_instance
+        return kwargs
     model = MapIdea
