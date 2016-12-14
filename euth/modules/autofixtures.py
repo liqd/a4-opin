a17bf30fa1dda5fd22ee01daffc632e3ac047ac8
Yves Müller <yves.mueller@liqd.de>
2016-12-14 11:47:46 +0100

    purge deadcode after switiching modules/phases

--- euth/modules/autofixtures.py
+++
@@ -1,9 +0,0 @@
-from autofixture import AutoFixture, register
-
-from .models import Module
-
-
-class ModuleAutoFixture(AutoFixture):
-    follow_pk = True
-
-register(Module, ModuleAutoFixture)
--- euth/phases/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core import exceptions
-
-from . import content
-
-
-def validate_content(identifier):
-    if identifier not in content:
-        raise exceptions.ValidationError('Specify a valid phase name')
-    return identifier
