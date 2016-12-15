bae9532f5fe4c722e0c37be550dbf5c66cac27f3
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-12-15 12:16:11 +0100

    add helptexts

--- euth/maps/models.py
+++ euth/maps/models.py
@@ -1,3 +1,5 @@
+from django.utils.translation import ugettext_lazy as _
+
 from euth.ideas import models as idea_models
 from euth.modules import models as module_models
 
@@ -15,7 +17,10 @@ class AreaSettings(module_models.AbstractSettings):
 
 
 class MapIdea(idea_models.Idea):
-    point = PointField()
+    point = PointField(
+        verbose_name=_('Where can your idea be located on a map?'),
+        help_text=_('Click inside marked area to set a marker. '
+                    'Drag and drop marker to change place.'))
 
     objects = idea_models.IdeaQuerySet.as_manager()
 
