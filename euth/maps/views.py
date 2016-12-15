60764104dc76060f3f0077e09639b58ea1072eb0
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-12-15 11:03:50 +0100

    add seetings to map idea update form

--- euth/maps/views.py
+++ euth/maps/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from euth.ideas import views as idea_views
 
 from . import forms
@@ -20,6 +21,12 @@ class MapIdeaCreateView(idea_views.IdeaCreateView):
 
 class MapIdeaUpdateView(idea_views.IdeaUpdateView):
     model = MapIdea
+    form_class = forms.MapIdeaForm
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['settings_instance'] = self.object.module.settings_instance
+        return kwargs
 
 
 class MapIdeaDeleteView(idea_views.IdeaDeleteView):
@@ -28,3 +35,10 @@ class MapIdeaDeleteView(idea_views.IdeaDeleteView):
 
 class MapIdeaDetailView(idea_views.IdeaDetailView):
     model = MapIdea
+    queryset = MapIdea.objects.annotate_positive_rating_count()\
+        .annotate_negative_rating_count()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['map_url'] = settings.BASE_MAP
+        return context
