7b23fc7679b7ed29f1ca21e9f85c8cbf44f16483
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-12-20 16:41:51 +0100

    add some styling

--- euth/maps/static/js/custom/map_display_points.js
+++ euth/maps/static/js/custom/map_display_points.js
@@ -1,4 +1,5 @@
 window.jQuery(document).ready(function () {
+  var $ = window.jQuery
   var L = window.L
   var polygon = window.polygon
   var point = window.point
@@ -7,7 +8,7 @@ window.jQuery(document).ready(function () {
   var basemap = baseurl + '{z}/{x}/{y}.png'
   var osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   var baselayer = L.tileLayer(basemap, { maxZoom: 18, attribution: osmAttrib })
-  var map = new L.Map('map')
+  var map = new L.Map('map', { scrollWheelZoom: false, zoomControl: false })
   baselayer.addTo(map)
 
   var polygonStyle = {
@@ -24,18 +25,32 @@ window.jQuery(document).ready(function () {
     }
 
   var basePolygon = L.geoJson(polygon, {style: polygonStyle}).addTo(map)
+  basePolygon.on('dblclick', function (event) {
+    map.zoomIn()
+  })
+  basePolygon.on('click', function (event) {
+    map.closePopup()
+  })
+
   map.fitBounds(basePolygon)
 
   function getImage (feature) {
     if (feature.properties.image) {
       return '<div class="maps-popups-popup-image" style="background-image:url(' + feature.properties.image + ');"></div>'
     }
-    return ''
+    return '<div class="maps-popups-popup-image"></div>'
   }
+
+  var icon = L.icon({
+    iconUrl: '/static/images/map_pin_01_2x.png',
+    iconSize: [30, 45],
+    iconAnchor: [15, 45],
+    popupAnchor: [0, -45]
+  })
 
   L.geoJson(point, {
     pointToLayer: function (feature, latlng) {
-      var marker = L.marker(latlng, {draggable: false}).addTo(map)
+      var marker = L.marker(latlng, {icon: icon}).addTo(map)
       var popupContent = getImage(feature) +
                         '<div class="maps-popups-popup-meta">' +
                             '<span class="idea-upvotes idea-meta-item">' +
@@ -49,9 +64,18 @@ window.jQuery(document).ready(function () {
                             '</span>' +
                             '</div>' +
                         '<div class="maps-popups-popup-name"><a href="' + feature.properties.url + '">' + feature.properties.name + '</a></div>'
-
       marker.bindPopup(popupContent, customOptions)
       return marker
     }
+  })
+
+  $('#zoom-in').click(function () {
+    map.setZoom(map.getZoom() + 1)
+    return false
+  })
+
+  $('#zoom-out').click(function () {
+    map.setZoom(map.getZoom() - 1)
+    return false
   })
 })
