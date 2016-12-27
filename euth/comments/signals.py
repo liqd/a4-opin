3c95364d2ffc2742a23ea6d1d27f139cd671b9ab
Yves Müller <yves.mueller@liqd.de>
2016-12-27 12:50:49 +0100

    Refactor signal setup to be more centralised

--- euth/comments/signals.py
+++ euth/comments/signals.py
@@ -1,22 +1,21 @@
+from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import post_delete
-from django.dispatch import receiver
 
-from adhocracy4.ratings import services as rating_services
-
-from . import services
 from .models import Comment
 
 
-@receiver(post_delete, sender=Comment)
-def delete_comments_for_Comment(sender, instance, **kwargs):
+def delete_content_object_handler(sender, instance, **kwargs):
     contenttype = ContentType.objects.get_for_model(instance)
     pk = instance.pk
-    services.delete_comments(contenttype, pk)
+    Comment.objects\
+           .filter(content_type=contenttype, object_pk=pk)\
+           .delete()
 
 
-@receiver(post_delete, sender=Comment)
-def delete_ratings_for_Comment(sender, instance, **kwargs):
-    contenttype = ContentType.objects.get_for_model(instance)
-    pk = instance.pk
-    rating_services.delete_ratings(contenttype, pk)
+# setup signal handlers for all commentables
+for commentable in settings.COMMENTABLES:
+    post_delete.connect(
+        delete_content_object_handler,
+        '.'.join(commentable)
+    )
--- euth/ideas/signals.py
+++ euth/ideas/signals.py
@@ -1,10 +1,7 @@
-from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import post_delete, post_init, post_save
 from django.dispatch import receiver
 
 from adhocracy4.images import services as image_services
-from adhocracy4.ratings import services as rating_services
-from euth.comments import services as comment_services
 
 from .models import Idea
 
@@ -24,17 +21,3 @@ def delete_old_image(sender, instance, **kwargs):
 @receiver(post_delete, sender=Idea)
 def delete_images_for_Idea(sender, instance, **kwargs):
     image_services.delete_images([instance.image])
-
-
-@receiver(post_delete, sender=Idea)
-def delete_comments_for_Idea(sender, instance, **kwargs):
-    contenttype = ContentType.objects.get_for_model(instance)
-    pk = instance.pk
-    comment_services.delete_comments(contenttype, pk)
-
-
-@receiver(post_delete, sender=Idea)
-def delete_ratings_for_Idea(sender, instance, **kwargs):
-    contenttype = ContentType.objects.get_for_model(instance)
-    pk = instance.pk
-    rating_services.delete_ratings(contenttype, pk)
