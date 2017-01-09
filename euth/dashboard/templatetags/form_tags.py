7afbecea97375e86ff9d9b981d300c5106c747fe
root <root@localhost.localdomain>
2017-01-09 17:50:31 -0500

    initial commit

--- euth/dashboard/templatetags/form_tags.py
+++ euth/dashboard/templatetags/form_tags.py
@@ -1,7 +1,23 @@
 from django import template
-
+import datetime
+import json
 register = template.Library()
 
+
+def print_timestamp(timestamp):
+    try:
+        #assume, that timestamp is given in seconds with decimal point
+        ts = float(timestamp)
+    except ValueError:
+        return None
+    print("timestamp: "+str(timestamp)+" date: "+str(datetime.datetime.fromtimestamp(ts)))
+    return str(datetime.datetime.fromtimestamp(ts))
+register.filter(print_timestamp)
+
+
+def get_simplejson(modelobject):
+    return json.dumps(modelobject)
+register.filter(get_simplejson)
 
 @register.assignment_tag
 def get_checkbox_label(form, fieldname):
--- euth_wagtail/settings/base.py
+++ euth_wagtail/settings/base.py
@@ -348,6 +348,10 @@ ACTIONABLE = [
 ]
 
 FLASHPOLL_URL = "https://opin.flashpoll.eu/"
+FLASHPOLL_URL = "https://opin.flashpoll.eu/"
+FLASHPOLL_BACK_URL = "http://int.backend.flashpoll.eu/flashpoll/api/v7/management"
+FLASHPOLL_BACK_USER = "fp_management"
+FLASHPOLL_BACK_PASSWORD = "53c4100e8ab143fe59fcb2e743cf4aba662ad25lacab0eb37fb9c69d8f27363fa19f0531bd681"
 
 BASE_MAP = 'https://{s}.tile.openstreetmap.org/'
 
