14e2ff786e94e79db2a0433195af604a338eefcb
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-01-12 12:13:25 +0100

    fix tests

--- tests/documents/conftest.py
+++ tests/documents/conftest.py
@@ -1,6 +1,6 @@
 from pytest_factoryboy import register
 
-from tests.comments import factories as comment_factories
+from tests.actions import factories as comment_factories
 from tests.documents import factories as document_fatories
 
 register(comment_factories.CommentFactory)
