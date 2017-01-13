b78dc2566a7bc455c59e4246067836521529d842
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-01-13 11:02:40 +0100

    fix branch

--- euth/offlinephases/migrations/0001_initial.py
+++ euth/offlinephases/migrations/0001_initial.py
@@ -2,14 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import django.utils.timezone
 import ckeditor.fields
+import django.utils.timezone
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('a4phases', '0004_auto_20170112_1333'),
+        ('a4phases', '0004_change_order'),
     ]
 
     operations = [
@@ -17,9 +17,9 @@ class Migration(migrations.Migration):
             name='Offlinephase',
             fields=[
                 ('created', models.DateTimeField(editable=False, default=django.utils.timezone.now)),
-                ('modified', models.DateTimeField(editable=False, blank=True, null=True)),
+                ('modified', models.DateTimeField(null=True, editable=False, blank=True)),
                 ('text', ckeditor.fields.RichTextField()),
-                ('phase', models.OneToOneField(primary_key=True, serialize=False, to='a4phases.Phase')),
+                ('phase', models.OneToOneField(primary_key=True, to='a4phases.Phase', serialize=False)),
             ],
             options={
                 'abstract': False,
