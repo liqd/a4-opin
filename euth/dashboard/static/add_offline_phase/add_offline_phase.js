8bf082fb412d3a5fbfee11554ef7c783c90a5e11
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-01-19 13:35:46 +0100

    fix accordiion and other things

--- euth/dashboard/static/add_offline_phase/add_offline_phase.js
+++ euth/dashboard/static/add_offline_phase/add_offline_phase.js
@@ -2,15 +2,13 @@ window.jQuery(document).ready(function () {
   var $ = window.jQuery
 
   function cloneMore (selector, type, target) {
-    var newElement = $(selector).clone(true)
+    var newElement = $(selector).clone()
     var total = $('#id_' + type + '-TOTAL_FORMS').val()
 
     newElement.find('#id_phases-0-type').val('euth_offlinephases:000:offline')
     newElement.find('.collapse').eq(0).text('Offline Phase ').append('<i class="fa fa-chevron-up pull-right"></i>')
 
-    var accordion = newElement.find('.collapse')
-    accordion.eq(0).attr('href', '#phase-' + (parseInt(total) + 1))
-    accordion.eq(1).attr('id', 'phase-' + (parseInt(total) + 1))
+    newElement.find('.update-offline-documentation').remove()
 
     newElement.find(':input').each(function () {
       var currentType = $(this).attr('name').split('-')[2]
@@ -18,6 +16,8 @@ window.jQuery(document).ready(function () {
         $(this).val('')
       }
       if (currentType === 'start_date' || currentType === 'end_date') {
+        $(this).attr('data-default-date', '')
+        $(this).val('')
         $(this).flatpickr()
       }
       if (currentType === 'id') {
@@ -29,6 +29,10 @@ window.jQuery(document).ready(function () {
     $(target).remove()
 
     $('.phase-form').each(function (index) {
+      var accordion = $(this).find('.collapse')
+      accordion.eq(0).attr('href', '#phase-' + index)
+      accordion.eq(1).attr('id', 'phase-' + index)
+
       $(this).find(':input').each(function () {
         var currentNumber = $(this).attr('name').split('-')[1]
         var currentType = $(this).attr('name').split('-')[2]
