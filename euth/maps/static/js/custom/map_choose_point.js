d36197ce3e10565fb040421d0b7fb910643bbc5d
vellip <vellerphilipp@googlemail.com>
2017-02-06 10:14:13 +0100

    map zooming

--- euth/maps/static/js/custom/map_choose_point.js
+++ euth/maps/static/js/custom/map_choose_point.js
@@ -1,8 +1,8 @@
 function createMap (L, baseurl, name) {
   var basemap = baseurl + '{z}/{x}/{y}.png'
   var osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  var baselayer = L.tileLayer(basemap, { maxZoom: 18, attribution: osmAttrib })
-  var map = new L.Map('map_' + name)
+  var baselayer = L.tileLayer(basemap, {maxZoom: 18, attribution: osmAttrib})
+  var map = new L.Map('map_' + name, {scrollWheelZoom: false})
   baselayer.addTo(map)
   return map
 }
