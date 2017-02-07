fbfb8f902c94db60c46e0c1d2fb058733ec541a8
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-02-07 14:45:05 +0100

    fix date after loading

--- euth/contrib/static/js/dateTimeInput.js
+++ euth/contrib/static/js/dateTimeInput.js
@@ -3,5 +3,9 @@ $('.flatpickr').flatpickr({
   onChange: function (selectedDates, dateStr, instance) {
     var newDate = instance.formatDate(instance.config.dateFormat, selectedDates[0].fp_toUTC())
     $(instance.element).val(newDate)
+  },
+  onReady: function (selectedDates, dateStr, instance) {
+    var newDate = instance.formatDate(instance.config.dateFormat, selectedDates[0].fp_toUTC())
+    $(instance.element).val(newDate)
   }
 })
