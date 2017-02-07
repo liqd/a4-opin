6964e2a5fa985b5cbcc29a303568eeb8c27a513f
vellip <vellerphilipp@googlemail.com>
2017-02-07 15:08:59 +0100

    add email notification to newly added moderator

--- euth/projects/api.py
+++ euth/projects/api.py
@@ -3,6 +3,8 @@ from rest_framework import permissions, viewsets
 from adhocracy4.projects.models import Project
 
 from euth.contrib.api.permissions import IsInitiatorOrSuperUser
+from euth.contrib.emails import send_email_with_template
+from euth.users.models import User
 from .serializers import ProjectSerializer
 
 
@@ -11,3 +13,22 @@ class ProjectViewSet(viewsets.mixins.UpdateModelMixin,
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     permission_classes = (permissions.IsAuthenticated, IsInitiatorOrSuperUser)
+
+    def perform_update(self, serializer):
+        moderators = serializer.instance.moderators.values_list('id',
+                                                                flat=True)
+        # create a set from current moderators and moderators after ajax
+        moderators = set(moderators)
+        post_moderators = set(serializer.initial_data['moderators'])
+        new_moderator = post_moderators - moderators
+        new_moderator = User.objects.get(id__exact=new_moderator.pop())
+
+        send_email_with_template(
+            [new_moderator.email],
+            'notify_new_moderator',
+            {
+                'project': serializer.instance,
+            }
+        )
+
+        serializer.save()
