a202421faa7c3fdbf9e6b67be2d0bc57163fccf5
Yves Müller <yves.mueller@liqd.de>
2017-02-20 13:46:08 +0100

    Add own rules set for mapidas
     - do not reuse permissions from ideas app, because it is non-standard
       at react_comments and react_rates count on permissions beeing there

--- euth/maps/rules.py
+++ euth/maps/rules.py
@@ -3,11 +3,36 @@ from rules.predicates import is_superuser
 
 from adhocracy4.modules.predicates import (is_context_initiator,
                                            is_context_member,
-                                           is_context_moderator)
-from adhocracy4.phases.predicates import phase_allows_create
-
+                                           is_context_moderator,
+                                           is_owner,
+                                           is_public_context)
+from adhocracy4.phases.predicates import (phase_allows_comment,
+                                          phase_allows_create,
+                                          phase_allows_modify,
+                                          phase_allows_rate)
 from .models import MapIdea
 
-rules.add_perm('euth_maps.propose_idea',
+
+rules.add_perm('euth_maps.rate_mapidea',
+               is_superuser | is_context_moderator | is_context_initiator |
+               (is_context_member & phase_allows_rate))
+
+
+rules.add_perm('euth_maps.comment_mapidea',
+               is_superuser | is_context_moderator | is_context_initiator |
+               (is_context_member & phase_allows_comment))
+
+
+rules.add_perm('euth_maps.modify_mapidea',
+               is_superuser | is_context_moderator | is_context_initiator |
+               (is_context_member & is_owner & phase_allows_modify))
+
+
+rules.add_perm('euth_maps.propose_mapidea',
                is_superuser | is_context_moderator | is_context_initiator |
                (is_context_member & phase_allows_create(MapIdea)))
+
+
+rules.add_perm('euth_maps.view_mapidea',
+               is_superuser | is_context_moderator | is_context_initiator |
+               is_context_member | is_public_context)
