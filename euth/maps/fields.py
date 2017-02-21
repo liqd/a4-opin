7d4df211c1b582dcc8ee214a1eeb5c3d820cb50b
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-02-21 15:42:19 +0100

    allow different messages for different fields

--- euth/maps/fields.py
+++ euth/maps/fields.py
@@ -2,13 +2,13 @@ from django.core import validators as django_validators
 from django.utils.translation import ugettext_lazy as _
 from jsonfield.fields import JSONField, JSONFormField
 
-from .validators import GeoJSONFormFieldValidator
-
 
 class GeoJSONFormField(JSONFormField):
 
     def __init__(self, *args, **kwargs):
+        required_message = kwargs.pop('required_message')
         super().__init__(*args, **kwargs)
+        self.default_error_messages['required'] = required_message
 
     def to_python(self, value):
         empty_featureset = '{"type":"FeatureCollection","features":[]}'
@@ -26,6 +26,7 @@ class GeoJSONField(JSONField):
     geom_type = 'GEOMETRY'
 
     def formfield(self, **kwargs):
+        kwargs.setdefault('required_message', self.required_message)
         return super(GeoJSONField, self).formfield(**kwargs)
 
 
@@ -35,7 +36,9 @@ class GeometryField(GeoJSONField):
 
 class PointField(GeometryField):
     geom_type = 'POINT'
+    required_message = _('Please add a Marker on the map')
 
 
 class MultiPolygonField(GeoJSONField):
     geom_type = 'MULTIPOLYGON'
+    required_message = _('Please draw a Polygon on the map')
