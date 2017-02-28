c5fd5b61db70f70a37b396e935caff8d065d47a5
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-02-28 14:04:05 +0100

    fix maps

--- euth/dashboard/views.py
+++ euth/dashboard/views.py
@@ -216,13 +216,14 @@ class DashboardProjectUpdateView(DashboardBaseMixin,
         qs = phase_models.Phase.objects.filter(module__project=self.object)
         kwargs['phases__queryset'] = qs
 
-        if qs.first().type.startswith('euth_flashpoll'):
-            settings_instance = qs.first().module.settings_instance
+        settings_instance = qs.first().module.settings_instance
+        if settings_instance:
             kwargs['module_settings__instance'] = settings_instance
-            self.kwargs['module_settings'] = 'euth_flashpoll'
-            self.kwargs['pollid'] = settings_instance.key
-        else:
-            self.kwargs['module_settings'] = 'default'
+            if qs.first().type.startswith('euth_flashpoll'):
+                self.kwargs['module_settings'] = 'euth_flashpoll'
+                self.kwargs['pollid'] = settings_instance.key
+            else:
+                self.kwargs['module_settings'] = 'default'
 
         return kwargs
 
