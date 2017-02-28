06b7c4b4e245fa40727e7a51d27b3769e469f1b3
Yves Müller <yves.mueller@liqd.de>
2017-02-28 09:59:57 +0100

    rewrite requirement spec to enum

--- euth/blueprintsuggest/forms.py
+++ euth/blueprintsuggest/forms.py
@@ -1,97 +1,64 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
+
+from . import blueprints
 
 
 class GetSuggestionForm(forms.Form):
     aim_choices = [
-        {
-            'value': 'collect_ideas',
-            'label': _('create and gather new ideas or visions.'),
-            'example': [_('(Urban) planning processes'),
-                        _('Develop concepts or guiding principles')]
-        }, {
-            'value': 'discuss_topic',
-            'label': _('gather feedback on a topic and discuss it in '
-                       'greater detail.'),
-            'example': [_('Discuss existing concepts or plans'),
-                        _('Develop solutions for existing problems')]
-        }, {
-            'value': 'design_place',
-            'label': _('design a place.'),
-            'example': [_('(Urban) planning processes'),
-                        _('Set the agenda of an event')]
-        }, {
-            'value': 'run_survey',
-            'label': _('learn about what people like most.'),
-            'example': [_('Majority votes'), _('Opinion polls')]
-        }, {
-            'value': 'run_competition',
-            'label': _('run a competition.'),
-            'example': [_('All sorts of competitions, '
-                          'like idea contests etc.')]
-        }, {
-            'value': 'work_document',
-            'label': _('work collaboratively on a text document.'),
-            'example': [_('Draft or revise statutes, articles, or charters'),
-                        _('Involve different authors in writing a shared '
-                          'text')]
-        },
+        {'value': a.value, 'label': a.label, 'example': a.examples}
+        for a in blueprints.Aim
     ]
 
     aim = forms.ChoiceField(
-        choices=[(choice['value'], choice['label']) for choice in aim_choices],
+        choices=[(aim.value, aim.label) for aim in blueprints.Aim],
         required=True
     )
 
     result = forms.ChoiceField(
-        choices=[
-            (2, _('Majority vote')),
-            (1, _('Both')),
-            (3, _('Weighted arguments')),
-        ],
+        choices=[(r.value, r.label) for r in blueprints.Result],
         widget=forms.RadioSelect,
         required=False,
         label=_('What is the desired outcome of the project?'),
     )
 
     experience = forms.ChoiceField(
-        choices=[
-            (1, _('More than 5 participative projects')),
-            (2, _('More than 2 participative projects')),
-            (3, _('1-2 partcipative projects')),
-            (4, _('I have no experiences in organising participative '
-                  'projects')),
-        ],
+        choices=[(e.value, e.label) for e in blueprints.Experience],
         widget=forms.RadioSelect,
         required=False,
         label=_('How many participative projects have you organised and '
                 'managed in the past?')
     )
 
-    dedication = forms.ChoiceField(
-        choices=[
-            (1, _('High dedication')),
-            (2, _('Medium dedication')),
-            (3, _('Low dedication')),
-            (4, _('No dedication')),
-            (3, _('I don\'t know.')),
-        ],
+    motivation = forms.ChoiceField(
+        choices=[(m.value, m.label) for m in blueprints.Motivation],
         required=False,
         widget=forms.RadioSelect,
         label=_('How dedicated are your participants?')
     )
 
     def clean_aim(self, *args, **kwargs):
-        available_aims = [
-            'collect_ideas',
-            'discuss_topic',
-            'design_place',
-            'run_survey',
-            'run_competition',
-        ]
-
-        if self.cleaned_data['aim'] not in available_aims:
+        try:
+            return blueprints.Aim(self.cleaned_data['aim'])
+        except KeyError:
             raise ValidationError(_('Invalid aim selected'))
 
-        return self.cleaned_data['aim']
+    def _clean_optional_enum(self, name, enum):
+        try:
+            str_value = self.cleaned_data[name]
+            if str_value:
+                return enum(int(str_value))
+            else:
+                return None
+        except (KeyError, ValueError):
+            raise ValidationError(_('Invalid selection'))
+
+    def clean_result(self, *args, **kwargs):
+        return self._clean_optional_enum('result', blueprints.Result)
+
+    def clean_experience(self, *args, **kwargs):
+        return self._clean_optional_enum('experience', blueprints.Experience)
+
+    def clean_motivation(self, *args, **kwargs):
+        return self._clean_optional_enum('motivation', blueprints.Motivation)
--- euth/blueprintsuggest/views.py
+++ euth/blueprintsuggest/views.py
@@ -1,12 +1,35 @@
 from django.core.urlresolvers import reverse
-from django.shortcuts import render
 from django.template import Context
 from django.views import generic
 from rules.contrib import views as rules_views
 
 from euth.dashboard.views import DashboardBaseMixin
-from . import forms as forms
-from .calculations import BlueprintSuggester
+
+from . import blueprints, forms
+
+
+def filter_blueprints(aim, result, experience, motivation):
+    candidates = []
+
+    for name, blueprint in blueprints.blueprints:
+        if aim not in blueprint.requirements.aims:
+            continue
+
+        requirements = blueprint.requirements
+
+        if result and experience and motivation:
+            if requirements.result.value > result.value:
+                continue
+            if requirements.experience.value > experience.value:
+                continue
+            if requirements.motivation.value > motivation.value:
+                continue
+        candidates.append((name, blueprint))
+
+    if not candidates:
+        candidates.append(blueprints.get_fallback_blueprint(aim))
+
+    return candidates
 
 
 class SuggestFormView(DashboardBaseMixin,
@@ -21,15 +44,18 @@ class SuggestFormView(DashboardBaseMixin,
                        kwargs={'organisation_slug': self.organisation.slug})
 
     def form_valid(self, form):
-        suggester = BlueprintSuggester(form.cleaned_data)
         context = {
-            'blueprints': suggester.get_blueprints(),
-            'request': self.request
+            'blueprints': filter_blueprints(**form.cleaned_data),
+            'organisation': self.organisation,
+            'request': self.request  # FIXME: should be done context processor
         }
         context.update(self.get_context_data())
         context = Context(context)
-        return render(self.request, 'euth_blueprintsuggest/result.html',
-                      context)
+        return self.response_class(
+            request=self.request,
+            template='euth_blueprintsuggest/result.html',
+            context=context
+        )
 
     def get_permission_object(self):
         return self.organisation
