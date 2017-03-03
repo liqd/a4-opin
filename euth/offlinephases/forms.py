4bae656d2b8f4b680614523a45945ed2dc0792a3
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-03-03 15:14:43 +0100

    add file upload widget

--- euth/offlinephases/forms.py
+++ euth/offlinephases/forms.py
@@ -4,24 +4,25 @@ from django.forms import modelformset_factory
 from contrib.multiforms import multiform
 
 from . import models as offlinephase_models
+from . import widgets
 
 
 class OfflinephaseForm(forms.ModelForm):
-
     class Meta:
         model = offlinephase_models.Offlinephase
         fields = ['text']
 
 
 class FileUploadForm(forms.ModelForm):
-
     class Meta:
         model = offlinephase_models.FileUpload
         fields = ['title', 'document']
+        widgets = {
+            'document': widgets.FileUploadWidget()
+        }
 
 
 class OfflinephaseMultiForm(multiform.MultiModelForm):
-
     base_forms = [
         ('offlinephase', OfflinephaseForm),
         ('fileuploads', modelformset_factory(
