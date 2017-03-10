4bbe861770663c6932e11224b2c644359ff3a363
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-03-10 15:54:49 +0100

    add euth_wagtail to files in find_files method

--- euth/contrib/management/commands/makemessages.py
+++ euth/contrib/management/commands/makemessages.py
@@ -1,11 +1,15 @@
+from os.path import relpath
 from django.core.management.commands import makemessages
 import adhocracy4
 import euth
+import euth_wagtail
 
 
 class Command(makemessages.Command):
-
     def find_files(self, root):
-        a4_path = adhocracy4.__path__[0]
-        euth_path = euth.__path__[0]
-        return super().find_files(a4_path) + super().find_files(euth_path)
+        a4_path = super().find_files(adhocracy4.__path__[0])
+        euth_path = super().find_files(relpath(euth.__path__[0]))
+        euth_wagtail_path = super().find_files(
+            relpath(euth_wagtail.__path__[0])
+        )
+        return a4_path + euth_path + euth_wagtail_path
