b23f4cb1be175a59ab642ba37e4586b4ba5c8f85
mkind <sascha.zinke@splone.com>
2017-04-18 17:28:43 +0200

    Adding filter widget for filtering archived projects in the organisation detail view.

--- euth/organisations/views.py
+++ euth/organisations/views.py
@@ -1,17 +1,30 @@
 from django.views import generic
 
+from euth.contrib import filters
+
 from . import models
+
 
 
 class OrganisationDetailView(generic.DetailView):
     model = models.Organisation
+    filter = filters.ArchivedFilter
 
     def visible_projects(self):
         projects = self.object.project_set.order_by('-created')
         if self.request.user in self.object.initiators.all():
             return projects.all()
         else:
-            return projects.filter(is_draft=False, is_archived=False)
+            if 'is_archived' in self.request.GET and \
+                    self.request.GET['is_archived'] == 'true':
+                filter_archived = True
+            else:
+                filter_archived = False
+
+            return projects.filter(
+                        is_draft=False,
+                        is_archived=filter_archived
+                    )
 
 
 class OrganisationListView(generic.ListView):
