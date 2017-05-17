f742665a5d8a45359781ea7e428782277d274f31
fuzzylogic2000 <katharina.albers@liqd.de>
2017-05-17 13:17:43 +0200

    tests fixed and written!

--- euth/contrib/middleware.py
+++ euth/contrib/middleware.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class TimezoneMiddleware(object):
 
     def process_request(self, request):
-        if request.user.is_authenticated():
+        if request.user.is_authenticated() and request.user.timezone:
             timezone.activate(pytz.timezone(request.user.timezone))
         else:
             timezone.deactivate()
