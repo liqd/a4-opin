dd04d2722a2d5c4010898e5b2ec63d0735f1e2cc
Yves Müller <yves.mueller@liqd.de>
2017-05-24 08:57:13 +0200

    Adapt document serializer and viewset to a4 api

--- euth/documents/api.py
+++ euth/documents/api.py
@@ -1,6 +1,7 @@
-from rest_framework import mixins, permissions, viewsets
+from rest_framework import mixins, viewsets
 
-from adhocracy4.api.permissions import IsModerator
+from adhocracy4.api.mixins import ModuleMixin
+from adhocracy4.api.permissions import ViewSetRulesPermission
 
 from .models import Document
 from .serializers import DocumentSerializer
@@ -8,9 +9,18 @@ from .serializers import DocumentSerializer
 
 class DocumentViewSet(mixins.CreateModelMixin,
                       mixins.UpdateModelMixin,
+                      ModuleMixin,
                       viewsets.GenericViewSet):
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
-    permission_classes = (
-        permissions.IsAuthenticated, IsModerator,
-    )
+    permission_classes = (ViewSetRulesPermission,)
+
+    def get_permission_object(self):
+        return self.module
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({
+            'module_pk': self.module_pk,
+        })
+        return context
