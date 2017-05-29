ccaf1f65237f0c828d2bfdec4fa8df38afa9a81c
mkind <sascha.zinke@splone.com>
2017-05-29 15:30:31 +0200

    Adding a test, that triggers the DST fallback. This resulted in a missing time needed calc which is now tested.

--- tests/blueprints/test_views.py
+++ tests/blueprints/test_views.py
@@ -93,3 +93,34 @@ def test_form_error_2(client, organisation):
     assert response.status_code == 200
     assert 'euth_blueprints/form.html' in templates_used(response)
     assert len(response.context_data['form'].errors) == 2
+
+
+@pytest.mark.django_db
+def test_form_regression_fallback(client, organisation):
+    user = organisation.initiators.first()
+    client.login(username=user.email, password='password')
+
+    url = reverse('blueprints-form', kwargs={
+        'organisation_slug': organisation.slug
+    })
+    data = {
+        'aim': 'collect_ideas',
+        'result': '1',
+        'motivation': '2',
+        'experience': '1',
+        'participants': '2',
+        'scope': '1',
+        'duration': '0',
+        'accessibility': '3'
+    }
+    response = client.post(url, data)
+
+    assert response.status_code == 200
+    assert 'euth_blueprints/result.html' in templates_used(response)
+    assert 'form' not in response.context_data
+    assert len(response.context_data['blueprints']) > 0
+
+    for b in response.context_data['blueprints']:
+        # verify that for every blueprint a name, the blueprint and a
+        # time is given
+        assert len(b) == 3
