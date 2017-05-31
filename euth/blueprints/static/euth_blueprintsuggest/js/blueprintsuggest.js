ba0916673f2f21a8756f7f000452b5589c369dbb
Yves Müller <yves.mueller@liqd.de>
2017-05-31 12:40:47 +0200

    Make dst js wizard code more generic

--- euth/blueprints/static/euth_blueprintsuggest/js/blueprintsuggest.js
+++ euth/blueprints/static/euth_blueprintsuggest/js/blueprintsuggest.js
@@ -2,65 +2,69 @@
 (function ($) {
   var blueprintsuggest = {
     init: function () {
-      this.$aim = $('input[name="aim"]')
-      this.$experience = $('input[name="experience"]')
-      this.$result = $('input[name="result"]')
-      this.$motivation = $('input[name="motivation"]')
-      this.$scope = $('input[name="scope"]')
-      this.$duration = $('input[name="duration"]')
-      this.$participants = $('input[name="participants"]')
-      this.$accessibility = $('input[name="accessibility"]')
-      this.$form = $('.blueprintsuggest')
-
       $('.js-continue').on('click', this.clickContinueHandler.bind(this))
       $('.js-back').on('click', this.clickBackHandler)
       $('.js-send').on('click', this.clickSendHandler.bind(this))
     },
 
+    validate: function ($tab) {
+      /*
+       * Ensure that for each group of radio buttons in $tab at least one is checked.
+       */
+      var $radioButtons = $tab.find('input[type=radio]')
+      var radioButtonsByName = {}
+      $radioButtons.map(function () {
+        var $this = $(this)
+        var name = $this.attr('name')
+
+        if (!radioButtonsByName.hasOwnProperty(name)) {
+          radioButtonsByName[name] = $()
+        }
+
+        radioButtonsByName[name] = radioButtonsByName[name].add($this)
+      })
+
+      for (var key in radioButtonsByName) {
+        if (radioButtonsByName.hasOwnProperty(key)) {
+          var $inputs = radioButtonsByName[key]
+          if (!$inputs.filter(':checked').length) {
+            return false
+          }
+        }
+      }
+
+      return true
+    },
+
     clickContinueHandler: function (e) {
       var $this = $(e.target)
       var $tab = $this.parents('.tab-pane')
-      var $checked = this.$aim.filter(':checked')
+      var isValid = this.validate($tab)
 
       // remove old errorlist
       $tab.find('.errorlist').remove()
 
-      if (!$checked.length) {
+      if (!isValid) {
         // there's no radio button checked, not valid, so add new errorlist
-        var text = django.gettext('Please pick an aim for your project.')
-        $tab.find('.dst-lightbox-progress').before(this.getErrorElement(text))
+        $tab.find('.dst-lightbox-progress').before(this.getErrorElement())
         return true
       }
 
       $tab.removeClass('active').next().addClass('active')
-
       return false
     },
 
     clickSendHandler: function (e) {
       var $this = $(e.target)
       var $tab = $this.parents('.tab-pane')
-      var $checkedExperience = this.$experience.filter(':checked')
-      var $checkedResult = this.$result.filter(':checked')
-      var $checkedMotivation = this.$motivation.filter(':checked')
-      var $checkedDuration = this.$duration.filter(':checked')
-      var $checkedParticipants = this.$participants.filter(':checked')
-      var $checkedScope = this.$scope.filter(':checked')
-      var $checkedAccessibility = this.$accessibility.filter(':checked')
+      var isValid = this.validate($tab)
 
       // remove old errorlist
       $tab.find('.errorlist').remove()
 
-      if (!$checkedExperience.length ||
-          !$checkedResult.length ||
-          !$checkedMotivation.length ||
-          !$checkedDuration.length ||
-          !$checkedScope.length ||
-          !$checkedParticipants.length ||
-          !$checkedAccessibility.length) {
+      if (!isValid) {
         // there is some radio button not checked, not valid, so add new errorlist
-        var text = django.gettext('Please set all values for your project.')
-        $tab.find('.dst-lightbox-progress').before(this.getErrorElement(text))
+        $tab.find('.dst-lightbox-progress').before(this.getErrorElement())
         e.preventDefault()
         return false
       }
@@ -68,7 +72,8 @@
       return true
     },
 
-    getErrorElement: function (text) {
+    getErrorElement: function () {
+      var text = django.gettext('Please set all values for your project.')
       return '<ul class="errorlist"><li>' + text + '</li></ul>'
     },
 
