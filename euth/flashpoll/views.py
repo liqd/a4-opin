2c450ae4f8b4a17a8f40f8ed71ab9dedce336af5
Richard Ewelle <r.ewelle@alfstore.com>
2017-06-29 15:21:44 -0400

    added result tab for end users

--- euth/flashpoll/views.py
+++ euth/flashpoll/views.py
@@ -1,6 +1,9 @@
+import json
+import requests
 from django.conf import settings
 from django.utils.translation import get_language
 from django.views import generic
+from requests.auth import HTTPBasicAuth
 
 from adhocracy4.projects import mixins
 
@@ -21,4 +24,33 @@ class FlashpollDetailView(mixins.ProjectMixin, generic.DetailView):
                 poll_id=self.get_object().key
             )
         }
+        context['pollid'] = self.get_object().key
+        context['module_settings'] = "euth_flashpoll"    
+
+        url_poll = '{base_url}/poll/{poll_id}'.format(
+            base_url=settings.FLASHPOLL_BACK_URL,
+            poll_id=context['pollid']
+        )
+
+        headers = {'Content-type': 'application/json'}
+        res = requests.get(url_poll,
+                        headers=headers,
+                        auth=HTTPBasicAuth(settings.FLASHPOLL_BACK_USER,
+                                            settings.FLASHPOLL_BACK_PASSWORD
+                                            ))
+        context['poll'] = json.loads(res.text)
+
+        url_poll = '{base_url}/poll/{poll_id}/result'.format(
+            base_url=settings.FLASHPOLL_BACK_URL,
+            poll_id=context['pollid']
+        )
+
+        headers = {'Content-type': 'application/json'}
+        res = requests.get(url_poll,
+                        headers=headers,
+                        auth=HTTPBasicAuth(settings.FLASHPOLL_BACK_USER,
+                                            settings.FLASHPOLL_BACK_PASSWORD
+                                            ))
+        context['pollresult'] = json.loads(res.text)
+
         return super().get_context_data(**context)
