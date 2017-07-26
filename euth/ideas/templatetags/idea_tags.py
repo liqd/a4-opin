5f2bc73ed216a107b844091823430be727e3b997
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-07-26 19:51:56 +0200

    add template_tag to check if module has ideas

--- euth/ideas/templatetags/idea_tags.py
+++ euth/ideas/templatetags/idea_tags.py
@@ -1,5 +1,7 @@
 from django import template
 
+from euth.ideas.models import Idea
+
 register = template.Library()
 
 
@@ -11,3 +13,8 @@ def get_range(number, listcount):
         return range(listcount-4, listcount+1)
     else:
         return range(number-2, number+3)
+
+
+@register.assignment_tag
+def is_idea_list(module):
+    return Idea.objects.filter(module=module).count() > 0
