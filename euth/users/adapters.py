de26f9704ce16e20bf66de76706ce1af8c4a15f5
fuzzylogic2000 <katharina.albers@liqd.de>
2017-10-30 13:50:29 +0100

    urlencode next parameter

--- euth/users/adapters.py
+++ euth/users/adapters.py
@@ -1,4 +1,5 @@
 import re
+from urllib.parse import quote
 
 from allauth.account.adapter import DefaultAccountAdapter
 from django.utils.http import is_safe_url
@@ -23,7 +24,7 @@ class EuthAccountAdapter(DefaultAccountAdapter):
     def get_email_confirmation_url(self, request, emailconfirmation):
         url = super().get_email_confirmation_url(request, emailconfirmation)
         if 'next' in request.POST and is_safe_url(request.POST['next']):
-            return '{}?next={}'.format(url, request.POST['next'])
+            return '{}?next={}'.format(url, quote(request.POST['next']))
         else:
             return url
 
@@ -37,6 +38,6 @@ class EuthAccountAdapter(DefaultAccountAdapter):
 
     def get_email_confirmation_redirect_url(self, request):
         if 'next' in request.GET and is_safe_url(request.GET['next']):
-            return request.GET['next']
+            return quote(request.GET['next'])
         else:
             return super().get_email_confirmation_redirect_url(request)
