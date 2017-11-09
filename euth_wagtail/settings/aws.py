49b281a188e5252957697541981d067bc4494c44
Yves Müller <yves.mueller@liqd.de>
2017-11-09 09:45:32 +0100

    [plt] Discriminate build and run time

--- euth_wagtail/settings/aws.py
+++ euth_wagtail/settings/aws.py
@@ -16,27 +16,38 @@ INSTALLED_APPS += [
     'allauth.socialaccount.providers.google',
 ]
 
+if environ.get('PLATFORM_ENVIRONMENT'):
+    run_time = True
+else:
+    run_time = False
+build_time = not run_time
+
+
 ALLOWED_HOSTS = [
     'localhost',
-    '{}-{}.eu.platfrom.sh'.format(
-        environ['PLATFORM_ENVIRONMENT'],
-        environ['PLATFORM_PROJECT']
-    ),
 ]
 
-relationships = os.environ['PLATFORM_RELATIONSHIPS']
-relationships = json.loads(str(base64.b64decode(relationships), 'utf-8'))
-db_settings = relationships['database'][0]
+if run_time:
+    ALLOWED_HOSTS.append(
+        '{}-{}.eu.platfrom.sh'.format(
+            environ['PLATFORM_ENVIRONMENT'],
+            environ['PLATFORM_PROJECT']
+        )
+    )
 
-DATABASES = {
-    "default": {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': db_settings['path'],
-        'USER': db_settings['username'],
-        'PASSWORD': db_settings['password'],
-        'HOST': db_settings['host'],
-        'PORT': db_settings['port'],
+    relationships = os.environ['PLATFORM_RELATIONSHIPS']
+    relationships = json.loads(str(base64.b64decode(relationships), 'utf-8'))
+    db_settings = relationships['database'][0]
+
+    DATABASES = {
+        "default": {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': db_settings['path'],
+            'USER': db_settings['username'],
+            'PASSWORD': db_settings['password'],
+            'HOST': db_settings['host'],
+            'PORT': db_settings['port'],
+        }
     }
-}
 
 SECRET_KEY = environ['PLATFORM_PROJECT_ENTROPY']
