0eb46ae3c3b046be51adb380cd603b7a15cf410b
Yves Müller <yves.mueller@liqd.de>
2017-12-04 16:45:27 +0100

    Set translation befor filters in org list
      - required because sorting and filtering translated attributes will
        otherwise produce one result per translation

--- euth/organisations/views.py
+++ euth/organisations/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.utils import translation
 
 from adhocracy4.filters import views as filter_views
 from adhocracy4.projects import models as project_models
@@ -34,3 +35,10 @@ class OrganisationListView(filter_views.FilteredListView):
     model = models.Organisation
     paginate_by = 12
     filter_set = organisation_filters.OrganisationFilterSet
+
+    def filter_kwargs(self):
+        kwargs = super().filter_kwargs()
+        qs = kwargs['queryset']
+        qs = qs.active_translations(translation.get_language())
+        kwargs['queryset'] = qs
+        return kwargs
