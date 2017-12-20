1677e27259d631e67f745818cc31c9f1eec397b5
Yves Müller <yves.mueller@liqd.de>
2017-12-20 13:32:29 +0100

    Move all static files from assets to static

--- euth/contrib/static/js/app.js
+++ euth/contrib/static/js/app.js
@@ -5,7 +5,7 @@ var ReactFollow = require('../../../follows/static/follows/react_follows.jsx')
 var ReactLanguageSwitch = require('../../../dashboard/static/language_switch/react_language_switch.jsx')
 var ReactUserList = require('../../../dashboard/static/user_list/react_user_list.jsx')
 
-require('../../../../euth_wagtail/assets/js/euth_wagtail')
+require('../../../../euth_wagtail/static/js/euth_wagtail')
 
 module.exports = {
   'renderComment': ReactComments.renderComment,
