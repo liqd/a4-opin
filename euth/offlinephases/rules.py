369d37a659ef6be56ee7196b2c59ea1ea642f7af
Magdalena Noffke <magdalena.noffke@liqd.de>
2018-02-22 15:51:50 +0100

    remove offlinephases

--- euth/offlinephases/rules.py
+++
@@ -1,8 +0,0 @@
-import rules
-from rules.predicates import is_superuser
-
-from .predicates import is_offlinephase_moderator
-
-rules.add_perm(
-    'euth_offlinephases.modify_offlinephase',
-    is_offlinephase_moderator | is_superuser)
