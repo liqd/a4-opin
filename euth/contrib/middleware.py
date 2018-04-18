f0d73d40f1c79072dcb1c25fc60d1e6496687cda
Irae Hueck Costa <i.hueck-costa@liqd.net>
2018-04-18 18:24:57 +0200

    unique factory usernames and emails

--- euth/contrib/middleware.py
+++ euth/contrib/middleware.py
@@ -2,6 +2,7 @@ import pytz
 from django.utils import timezone
 from django.utils.deprecation import MiddlewareMixin
 
+
 class TimezoneMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
