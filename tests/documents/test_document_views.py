dee6eb845a50777b9662375e9614e7ce2884444c
Magdalena Noffke <magdalena.noffke@liqd.de>
2018-06-05 15:44:39 +0200

    add tests

--- tests/documents/test_document_views.py
+++ tests/documents/test_document_views.py
@@ -1,6 +1,8 @@
 import pytest
 from django.core.urlresolvers import reverse
 
+from adhocracy4.comments.models import Comment
+from euth.exports import mixins
 from tests.helpers import redirect_target
 
 
@@ -32,3 +34,19 @@ def test_paragraph_private_detail_view(client, paragraph_factory, user):
     paragraph.project.participants.add(user)
     response = client.get(url)
     response.status_code = 200
+
+
+@pytest.mark.django_db
+def test_reply_to_mixin(comment_factory, document):
+    mixin = mixins.ItemExportWithRepliesToMixin()
+
+    virtual = mixin.get_virtual_fields({})
+    assert 'replies_to' in virtual
+
+    comment = comment_factory(content_object=document)
+    reply_comment = comment_factory(content_object=comment)
+
+    assert Comment.objects.count() == 2
+
+    assert mixin.get_replies_to_data(comment) == ''
+    assert mixin.get_replies_to_data(reply_comment) == comment.id
