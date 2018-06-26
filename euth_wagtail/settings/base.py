a90cc7e7e90295f46790322ce1cac940830c7767
fuzzylogic2000 <katharina.albers@liqd.de>
2018-06-26 14:54:11 +0200

    more removing flashpoll

--- euth_wagtail/settings/base.py
+++ euth_wagtail/settings/base.py
@@ -398,10 +398,6 @@ ACTIONABLE = [
     ('a4comments', 'Comment')
 ]
 
-FLASHPOLL_URL = "https://opin.flashpoll.eu/"
-FLASHPOLL_BACK_URL = "http://backend.flashpoll.eu/flashpoll/api/v7/management"
-FLASHPOLL_BACK_USER = "fp_management"
-FLASHPOLL_BACK_PASSWORD = "53c4100e8ab143fe59fcb2e743cf4aba662ad25lacab0eb37fb9c69d8f27363fa19f0531bd681"
 GOOGLE_API_KEY = "AIzaSyC8kq3VbEzLA1xqe0ItRk-y4bgAg89h4Qc"
 
 A4_MAP_ATTRIBUTION = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
