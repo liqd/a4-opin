b9c8f857477dc4f24a15632f4b14537bfd3b3533
Robert Mader <robert.mader@posteo.de>
2020-04-09 17:29:54 +0200

    euth/maps: Use A4 fields

--- euth/maps/migrations/0002_add_helptexts.py
+++ euth/maps/migrations/0002_add_helptexts.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import euth.maps.fields
+import adhocracy4.maps.fields
 
 
 class Migration(migrations.Migration):
@@ -15,6 +15,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='mapidea',
             name='point',
-            field=euth.maps.fields.PointField(verbose_name='Where can your idea be located on a map?', help_text='Click inside marked area to set a marker. Drag and drop marker to change place.'),
+            field=adhocracy4.maps.fields.PointField(verbose_name='Where can your idea be located on a map?', help_text='Click inside marked area to set a marker. Drag and drop marker to change place.'),
         ),
     ]
