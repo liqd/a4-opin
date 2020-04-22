bed7ba5cb3a4f8af22c4accefa3e4a8f4e729cd3
Phillippa <p.morland@liqd.net>
2020-04-22 13:42:09 +0200

    assets: Move and update app.js

---
+++ euth_wagtail/assets/js/app.js
@@ -0,0 +1,34 @@
+/* global location */
+
+import 'bootstrap'
+
+import './euth_wagtail'
+
+import {
+  comments as ReactComments,
+  polls as ReactPolls,
+  ratings as ReactRatings,
+  widget as ReactWidget
+} from 'adhocracy4'
+
+import * as ReactParagraphs from '../../../euth/documents/static/documents/ParagraphBox.jsx'
+import * as ReactFollows from '../../../euth/follows/static/follows/react_follows.jsx'
+import * as ReactLanguageSwitch from '../../../euth/dashboard/static/language_switch/react_language_switch.jsx'
+import * as ReactUserList from '../../../euth/dashboard/static/user_list/react_user_list.jsx'
+
+function init () {
+  ReactWidget.initialise('a4', 'comment', ReactComments.renderComment)
+  ReactWidget.initialise('a4', 'ratings', ReactRatings.renderRatings)
+  ReactWidget.initialise('a4', 'polls', ReactPolls.renderPolls)
+  ReactWidget.initialise('a4', 'poll-management', ReactPolls.renderPollManagement)
+  ReactWidget.initialise('euth', 'document', ReactParagraphs.renderParagraphs)
+  ReactWidget.initialise('euth', 'follows', ReactFollows.renderFollow)
+  ReactWidget.initialise('euth', 'userlist', ReactUserList.renderUserList)
+  ReactWidget.initialise('euth', 'language-switch', ReactLanguageSwitch.renderLanguageSwitch)
+}
+
+document.addEventListener('DOMContentLoaded', init, false)
+
+export function getCurrentPath () {
+  return location.pathname
+}
