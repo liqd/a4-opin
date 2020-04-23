566f718cdc30aae74bf5e22c72e3c094158c0233
fuzzylogic2000 <katharina.albers@liqd.de>
2020-04-23 17:03:10 +0200

    apps/communitydebate: add phase and blueprint

---
+++ euth/communitydebate/phases.py
@@ -0,0 +1,22 @@
+from django.utils.translation import ugettext_lazy as _
+
+from adhocracy4 import phases
+
+from . import apps, models, views
+
+
+class DebatePhase(phases.PhaseContent):
+    app = apps.Config.label
+    phase = 'debate'
+    view = views.TopicListView
+
+    name = _('Debate phase')
+    description = _('Add topics and debate them.')
+    module_name = _('communitydebate')
+    icon = 'far fa-comments'
+
+    features = {
+        'crud': (models.Topic,),
+    }
+
+phases.content.register(DebatePhase())
