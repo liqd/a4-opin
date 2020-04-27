78b207e0f6694c5d5f332048cf54ec2b2a6f2bf6
Rineee <k.schmid@liqd.net>
2020-04-27 09:24:41 +0200

    euth/communitydebate: add categories to topic and make it commentable, rateable

--- euth/communitydebate/models.py
+++ euth/communitydebate/models.py
@@ -1,15 +1,33 @@
 from autoslug import AutoSlugField
 from ckeditor.fields import RichTextField
+from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 from django.urls import reverse
 
+from adhocracy4.categories.fields import CategoryField
+from adhocracy4.comments import models as comment_models
+from adhocracy4.models import query
 from adhocracy4.modules import models as module_models
+from adhocracy4.ratings import models as rating_models
+
+
+class TopicQuerySet(query.RateableQuerySet, query.CommentableQuerySet):
+    pass
 
 
 class Topic(module_models.Item):
     slug = AutoSlugField(populate_from='name', unique=True)
     name = models.CharField(max_length=120)
     description = RichTextField()
+    ratings = GenericRelation(rating_models.Rating,
+                              related_query_name='topic',
+                              object_id_field='object_pk')
+    comments = GenericRelation(comment_models.Comment,
+                               related_query_name='topic',
+                               object_id_field='object_pk')
+    category = CategoryField()
+
+    objects = TopicQuerySet.as_manager()
 
     def __str__(self):
         return self.name
