c9ec4e3e8184a11dd722ea6c69716c9ade0d3113
Rineee <k.schmid@liqd.net>
2020-04-27 12:15:25 +0200

    euth/communitydebate: add rating and commenting to debate phase

--- euth/communitydebate/rules.py
+++ euth/communitydebate/rules.py
@@ -5,9 +5,17 @@ from adhocracy4.modules.predicates import (is_context_initiator,
                                            is_context_member,
                                            is_context_moderator, is_owner,
                                            is_public_context)
-from adhocracy4.phases.predicates import phase_allows_add, phase_allows_change
+from adhocracy4.phases.predicates import (phase_allows_add,
+                                          phase_allows_change,
+                                          phase_allows_comment,
+                                          phase_allows_rate)
 
 from .models import Topic
+
+rules.add_perm('euth_communitydebate.comment_topic',
+               is_superuser | is_context_moderator | is_context_initiator |
+               (is_context_member & phase_allows_comment))
+
 
 rules.add_perm('euth_communitydebate.modify_topic',
                is_superuser | is_context_moderator | is_context_initiator |
@@ -17,6 +25,11 @@ rules.add_perm('euth_communitydebate.modify_topic',
 rules.add_perm('euth_communitydebate.propose_topic',
                is_superuser | is_context_moderator | is_context_initiator |
                (is_context_member & phase_allows_add(Topic)))
+
+
+rules.add_perm('euth_communitydebate.rate_topic',
+               is_superuser | is_context_moderator | is_context_initiator |
+               (is_context_member & phase_allows_rate))
 
 
 rules.add_perm('euth_communitydebate.view_topic',
