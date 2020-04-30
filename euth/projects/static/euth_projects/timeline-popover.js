dca4f86aba061fd8f15c43ffc63b85410100191c
Phillippa <p.morland@liqd.net>
2020-04-30 19:03:25 +0200

    templates/project_detail: rebuilding popover so it works

--- euth/projects/static/euth_projects/timeline-popover.js
+++ euth/projects/static/euth_projects/timeline-popover.js
@@ -5,12 +5,12 @@ $(function () {
   $('[data-toggle="popover"]').popover()
 })
 
-$('body').on('click', function (e) {
-  $('[data-toggle="popover"]').each(function () {
-    if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
-      $(this).popover('hide')
-    }
-  })
-})
+// $('body').on('click', function (e) {
+//   $('[data-toggle="popover"]').each(function () {
+//     if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+//       $(this).popover('hide')
+//     }
+//   })
+// })
 
 $('.dropdown-toggle').dropdown()
