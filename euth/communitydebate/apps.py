e10687023f7efd5272ffad70c1d1294b025ae507
Rineee <k.schmid@liqd.net>
2020-05-13 17:31:38 +0200

    euth/communitydebate: remove file on server when topic upload file instance is deleted/changed

--- euth/communitydebate/apps.py
+++ euth/communitydebate/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class Config(AppConfig):
     name = 'euth.communitydebate'
     label = 'euth_communitydebate'
+
+    def ready(self):
+        import euth.communitydebate.signals  # noqa:F401
