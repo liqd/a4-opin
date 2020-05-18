7fe48d92f8e6b7716ca140452797faa7f396aced
Rineee <k.schmid@liqd.net>
2020-05-18 11:21:53 +0200

    euth/offlinephases: add permission required mixin to offlineevent detail view

--- euth/offlinephases/rules.py
+++ euth/offlinephases/rules.py
@@ -1,8 +1,17 @@
 import rules
 from rules.predicates import is_superuser
 
+from adhocracy4.modules.predicates import (is_context_initiator,
+                                           is_context_member,
+                                           is_context_moderator,
+                                           is_public_context)
+
 from .predicates import is_offlinephase_moderator
 
 rules.add_perm(
     'euth_offlinephases.modify_offlinephase',
     is_offlinephase_moderator | is_superuser)
+
+rules.add_perm('euth_offlinephases.view_offlineevent',
+               is_superuser | is_context_moderator | is_context_initiator |
+               is_context_member | is_public_context)
