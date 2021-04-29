e5d2ec24c3e7b6579ba56892acad1b2caf1f78e8
Rine <k.schmid@liqd.net>
2021-04-29 12:10:06 +0200

    projects: add overwrite for access labels

---
+++ euth/projects/overwrites.py
@@ -0,0 +1,14 @@
+from django.utils.translation import ugettext_lazy as _
+
+from adhocracy4.projects.enums import Access
+
+
+def overwrite_access_enum_label():
+    Access.__labels__ = {
+        Access.PRIVATE: _('Only invited users can see content and can '
+                          'participate (private).'),
+        Access.PUBLIC: _('All users can see content and can participate '
+                         '(public).'),
+        Access.SEMIPUBLIC: _('All users can see content, only invited users '
+                             'can participate (semi-public).')
+    }
