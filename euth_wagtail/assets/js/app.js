bc756701e5655bc815e333319b403d8e19332426
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-07-12 15:51:42 +0200

    deps/update to bs5: use our own wide button instead of btn-block

--- euth_wagtail/assets/js/app.js
+++ euth_wagtail/assets/js/app.js
@@ -36,7 +36,6 @@ export function getCurrentPath () {
 }
 
 // enabling bootstrap popovers
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
+$(function () {
+  $('[data-toggle="popover"]').popover()
 })
