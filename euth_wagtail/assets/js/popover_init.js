1a57e936797bb1f1f8bef4ae2564b2738de210dc
phillimorland <5871230+phillimorland@users.noreply.github.com>
2021-07-13 12:45:23 +0200

    deps/update to bs5: add popover init file and load on appropriate page

---
+++ euth_wagtail/assets/js/popover_init.js
@@ -0,0 +1,7 @@
+import * as bootstrap from 'bootstrap'
+
+// enabling bootstrap popovers
+const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
