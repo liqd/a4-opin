ebbd93323f10d7b13ca4b06c38c5ceb4cc299fda
Kha <k.tran@liqd.net>
2021-07-22 14:04:15 +0200

    register/howto: rework after review

--- euth/contrib/templatetags/contrib_tags.py
+++ euth/contrib/templatetags/contrib_tags.py
@@ -1,5 +1,4 @@
 from django import template
-from django.utils.translation import ugettext_lazy as _
 
 register = template.Library()
 
@@ -25,39 +24,3 @@ def limited_paginator(page_obj):
         current_index < last_index - ADDITIONAL_PAGES else last_index
 
     return page_obj.paginator.page_range[start_index:end_index]
-
-
-@register.simple_tag
-def get_howto_items():
-    steps_header = 'How can I register my organisation and start a project?'
-    step1_text = 'Register as a user by following the steps on the left to '\
-        'create an account.'
-    step2_text = 'Send us en e-mail via info@opin.me and provide your '\
-        'username, email address, organisation name and its location, '\
-        'a short description of your project.'
-    step3_text = 'Since we are in beta for the moment, we cannot accept all'\
-        'requests to use OPIN. You will receive a reply within 48 hours.'
-    steps = [
-        {
-            'num': 1,
-            'header': _(steps_header),
-            'img': 'images/step1-2x-noshadow.png',
-            'subheader': _('1. Register an account'),
-            'text': _(step1_text)
-        },
-        {
-            'num': 2,
-            'header': _(steps_header),
-            'img': 'images/step2-2x-noshadow.png',
-            'subheader': _('2. Contact us'),
-            'text': _(step2_text)
-        },
-        {
-            'num': 3,
-            'header': _(steps_header),
-            'img': 'images/step3-2x-noshadow.png',
-            'subheader': _('3. We will review your request.'),
-            'text': _(step3_text)
-        },
-    ]
-    return steps
