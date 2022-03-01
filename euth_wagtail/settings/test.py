cf6b8c5d623d44933732ab7cc306b3acc0c446cf
rine <k.schmid@liqd.net>
2022-03-01 09:36:28 +0100

    tests//settings: add captcha to form data in tests

--- euth_wagtail/settings/test.py
+++ euth_wagtail/settings/test.py
@@ -10,3 +10,5 @@ A4_ORGANISATION_FACTORY = 'tests.organisations.factories.OrganisationFactory'
 A4_USER_FACTORY = 'tests.factories.UserFactory'
 
 ACCOUNT_EMAIL_VERIFICATION = 'optional'
+
+CAPTCHA_TEST_ACCEPTED_ANSWER = 'testpass'
