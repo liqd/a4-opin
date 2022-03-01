91797108d2f75282aecb82130aa2ae53d5a6a5cc
rine <k.schmid@liqd.net>
2022-03-01 13:43:17 +0100

    settings/dev: remove import of debug_toolbar

--- euth_wagtail/settings/dev.py
+++ euth_wagtail/settings/dev.py
@@ -13,15 +13,10 @@ SECRET_KEY = 'b*1ljsb!x7@d_o$sohx-&q-7n*#r=lwhy542zxk(e=fj%ey3xp'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 CAPTCHA_URL = 'https://captcheck.netsyms.com/api.php'
 
-try:
-    import debug_toolbar
-except ImportError:
-    pass
-else:
-    INSTALLED_APPS += ['debug_toolbar']
-    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+INSTALLED_APPS += ['debug_toolbar']
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
 
-    INTERNAL_IPS = ('127.0.0.1', 'localhost')
+INTERNAL_IPS = ('127.0.0.1', 'localhost')
 
 ADHOCRACY_SDK = "http://localhost:6551/static/js/AdhocracySDK.js"
 ADHOCRACY_URL = "http://localhost:6551"
