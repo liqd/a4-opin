1aac1831032767ed8b3d6a7d292c7509438a07ba
phillimorland <5871230+phillimorland@users.noreply.github.com>
2022-04-12 15:00:08 +0200

    FIXME: adding inline documentation to code and noting possible obsolete code

--- euth_wagtail/assets/js/popover_init.js
+++ euth_wagtail/assets/js/popover_init.js
@@ -1,5 +1,6 @@
 import * as bootstrap from 'bootstrap'
 
+// FIXME do we need this?
 // enabling bootstrap popovers
 const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 popoverTriggerList.map(function (popoverTriggerEl) {
