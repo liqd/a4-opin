8423894812934d2c3ceeca07d2b247f3819b8e1c
Mara Karagianni <m.karagianni@liqd.net>
2024-01-10 19:01:32 +0200

    euth, euth_wagtail: disable registrations

--- euth_wagtail/settings/base.py
+++ euth_wagtail/settings/base.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     'micawber.contrib.mcdjango',
 
     'adhocracy4.administrative_districts',
