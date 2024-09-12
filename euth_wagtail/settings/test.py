c12fa0955ce50d9e8af75a7cc665071d4de763d9
Julian Dehm <j.dehm@liqd.net>
2024-09-12 12:28:59 +0200

    cleanup tests, remove unused stuff in user app

--- euth_wagtail/settings/test.py
+++ euth_wagtail/settings/test.py
@@ -1,11 +1,5 @@
 from .dev import *
 
-INSTALLED_APPS += [
-    'tests.apps.blog.apps.BlogConfig',
-    'tests.apps.fakeprojects.apps.FakeProjectsConfig'
-
-]
-
 A4_ORGANISATION_FACTORY = 'tests.organisations.factories.OrganisationFactory'
 A4_USER_FACTORY = 'tests.factories.UserFactory'
 
--- tests/apps/blog/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from adhocracy4.models import base
-
-
-class Post(base.TimeStampedModel):
-
-    heading = models.CharField(max_length=200)
-    body = models.TextField()
