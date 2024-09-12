12c09761fa8df114f3280bca2a3d02460252bb46
Julian Dehm <j.dehm@liqd.net>
2024-09-12 10:23:51 +0200

    remove more unused stuff

--- euth_wagtail/assets/js/app.js
+++ euth_wagtail/assets/js/app.js
@@ -4,29 +4,7 @@ import 'bootstrap' // load bootstrap components
 
 import './euth_wagtail'
 
-// expose react components
-import {
-  commentsAsync as ReactCommentsAsync,
-  polls as ReactPolls,
-  ratings as ReactRatings,
-  widget as ReactWidget
-} from 'adhocracy4'
-
-import * as ReactParagraphs from '../../../euth/documents/static/documents/ParagraphBox.jsx'
-import * as ReactFollows from '../../../euth/follows/static/follows/react_follows.jsx'
-import * as ReactLanguageSwitch from '../../../euth/dashboard/static/language_switch/react_language_switch.jsx'
-import * as ReactUserList from '../../../euth/dashboard/static/user_list/react_user_list.jsx'
-
 function init () {
-  ReactWidget.initialise('a4', 'comment_async', ReactCommentsAsync.renderComment)
-  ReactWidget.initialise('a4', 'ratings', ReactRatings.renderRatings)
-  ReactWidget.initialise('a4', 'polls', ReactPolls.renderPolls)
-  ReactWidget.initialise('a4', 'poll-management', ReactPolls.renderPollManagement)
-
-  ReactWidget.initialise('euth', 'document', ReactParagraphs.renderParagraphs)
-  ReactWidget.initialise('euth', 'follows', ReactFollows.renderFollow)
-  ReactWidget.initialise('euth', 'userlist', ReactUserList.renderUserList)
-  ReactWidget.initialise('euth', 'language-switch', ReactLanguageSwitch.renderLanguageSwitch)
 }
 
 document.addEventListener('DOMContentLoaded', init, false)
