6f7e1b03e77b734c84ac22786e168cd3c52ca422
Julian Dehm <j.dehm@liqd.net>
2024-10-23 15:35:59 +0200

    remove or unused stuff from user app

--- euth/users/static/users/js/user_timezone.js
+++
@@ -1,4 +0,0 @@
-(function ($) {
-  const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone
-  $('#id_timezone').val(userTimezone)
-})(window.jQuery)
--- euth/users/templatetags/avatar.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.simple_tag()
-def get_avatar(user, size_alias):
-    return user.avatar_fallback
